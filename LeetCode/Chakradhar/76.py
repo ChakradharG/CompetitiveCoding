5e76c68359e426bdde16bb91149182a563839a0f
Chakradhar Guntuboina <47364794+ChakradharG@users.noreply.github.com>
2023-10-15 05:53:00 +0000

    76

--- LeetCode/Chakradhar/76.py
+++ LeetCode/Chakradhar/76.py
@@ -1,28 +1,29 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        def eq(d1, d2):
-            for k, v in d1.items():
-                if d2.get(k, 0) < v:
-                    return False
-            return True
-
-
         m, n = len(s), len(t)
         dt, ds = {}, {}
         wLen, left, right = float('inf'), 0, 0
 
         for i in range(n):
             dt[t[i]] = 1 + dt.get(t[i], 0)
+            ds[t[i]] = 0
+        have, need = 0, len(dt.keys())
 
         l, r = 0, 1
         while r <= m:
-            ds[s[r-1]] = 1 + ds.get(s[r-1], 0)
-            while eq(dt, ds):
-                if wLen > r - l:
-                    left, right = l, r
-                    wLen = r - l
-                ds[s[l]] -= 1
-                l += 1
+            if s[r-1] in ds:
+                ds[s[r-1]] = 1 + ds.get(s[r-1], 0)
+                if ds[s[r-1]] == dt[s[r-1]]:
+                    have += 1
+                while have == need:
+                    if wLen > r - l:
+                        left, right = l, r
+                        wLen = r - l
+                    if s[l] in ds:
+                        ds[s[l]] -= 1
+                        if ds[s[l]] < dt[s[l]]:
+                            have -= 1
+                    l += 1
             r += 1
 
         return s[left:right]
