e6d370e8a1a9a0ffed25baa6c039f145cfcc9a0f
Chakradhar Guntuboina <47364794+ChakradharG@users.noreply.github.com>
2023-10-15 04:58:00 +0000

    76

--- LeetCode/Chakradhar/76.py
+++ LeetCode/Chakradhar/76.py
@@ -1,8 +1,6 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
         def eq(d1, d2):
-            if len(d1.keys()) > len(d2.keys()):
-                return False
             for k, v in d1.items():
                 if d2.get(k, 0) < v:
                     return False
@@ -15,16 +13,19 @@ class Solution:
 
         for i in range(n):
             dt[t[i]] = 1 + dt.get(t[i], 0)
+            ds[t[i]] = 0
 
         l, r = 0, 1
         while r <= m:
-            ds[s[r-1]] = 1 + ds.get(s[r-1], 0)
-            while eq(dt, ds):
-                if wLen > r - l:
-                    w = s[l:r]
-                    wLen = r - l
-                ds[s[l]] -= 1
-                l += 1
+            if s[r-1] in ds:
+                ds[s[r-1]] += 1
+                while eq(dt, ds):
+                    if wLen > r - l:
+                        w = s[l:r]
+                        wLen = r - l
+                    if s[l] in ds:
+                        ds[s[l]] -= 1
+                    l += 1
             r += 1
 
         return w
