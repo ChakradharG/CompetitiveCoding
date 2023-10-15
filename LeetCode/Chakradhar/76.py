59352ba0b7cea043a9a35af700de210f9e04f0b4
Chakradhar Guntuboina <47364794+ChakradharG@users.noreply.github.com>
2023-10-15 04:52:00 +0000

    76

--- LeetCode/Chakradhar/76.py
+++ LeetCode/Chakradhar/76.py
@@ -1,29 +1,29 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        def ord_(c):
-            return ord(c) - 65
-
         def eq(d1, d2):
-            for i in range(58):
-                if d2[i] < d1[i]:
+            if len(d1.keys()) > len(d2.keys()):
+                return False
+            for k, v in d1.items():
+                if d2.get(k, 0) < v:
                     return False
             return True
 
+
         m, n = len(s), len(t)
-        dt, ds = [0 for i in range(58)], [0 for i in range(58)]
+        dt, ds = {}, {}
         w, wLen = '', float('inf')
 
         for i in range(n):
-            dt[ord_(t[i])] += 1
+            dt[t[i]] = 1 + dt.get(t[i], 0)
 
         l, r = 0, 1
         while r <= m:
-            ds[ord_(s[r-1])] += 1
+            ds[s[r-1]] = 1 + ds.get(s[r-1], 0)
             while eq(dt, ds):
                 if wLen > r - l:
                     w = s[l:r]
                     wLen = r - l
-                ds[ord_(s[l])] -= 1
+                ds[s[l]] -= 1
                 l += 1
             r += 1
 
