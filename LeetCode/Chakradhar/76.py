2492f2d66450621a84de73d514a31599326b3b5e
Chakradhar Guntuboina <47364794+ChakradharG@users.noreply.github.com>
2023-10-15 04:24:00 +0000

    76

--- LeetCode/Chakradhar/76.py
+++ LeetCode/Chakradhar/76.py
@@ -1,31 +1,29 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        m, n = len(s), len(t)
         def eq(d1, d2):
             for k, v in d1.items():
                 if d2.get(k, 0) < v:
                     return False
             return True
 
-        def cnt(string, left, right):
-            d = {}
-            for i in range(left, right):
-                d[string[i]] = 1 + d.get(string[i], 0)
-            return d
 
-        dt = cnt(t, 0, n)
+        m, n = len(s), len(t)
+        dt = {}
+        for i in range(0, n):
+            dt[t[i]] = 1 + dt.get(t[i], 0)
+
         wLen = float('inf')
         w = ""
         l, r = 0, 1
-        dw = {}
+        ds = {}
 
         while r <= m:
-            dw[s[r-1]] = 1 + dw.get(s[r-1], 0)
-            while eq(dt, dw):
+            ds[s[r-1]] = 1 + ds.get(s[r-1], 0)
+            while eq(dt, ds):
                 if wLen > r - l:
                     w = s[l:r]
                     wLen = r - l
-                dw[s[l]] -= 1
+                ds[s[l]] -= 1
                 l += 1
             r += 1
 
