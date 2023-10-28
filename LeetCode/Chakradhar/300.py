30b96efbc053b12874c4d3a270e898e17bda58b5
Chakradhar Guntuboina <47364794+ChakradharG@users.noreply.github.com>
2023-10-28 21:50:00 +0000

    300

--- LeetCode/Chakradhar/300.py
+++ LeetCode/Chakradhar/300.py
@@ -4,22 +4,19 @@ class Solution:
             start, end = 0, n
             while start < end:
                 mid = (start + end) // 2
-                if num == lis[mid]:
+                if num == nums[mid]:
                     return mid
-                elif num < lis[mid]:
+                elif num < nums[mid]:
                     end = mid
                 else:
                     start = mid + 1
-
             return start
 
-        lis, n = [], 0
+        n = 0
         for num in nums:
             idx = findIdx(num)
             if idx == n:
-                lis.append(num)
                 n += 1
-            else:
-                lis[idx] = num
+            nums[idx] = num
 
         return n
