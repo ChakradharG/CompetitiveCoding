725f2f995dfacda1c40c5d4cd4f8d18b685f3000
Chakradhar Guntuboina <47364794+ChakradharG@users.noreply.github.com>
2023-11-01 20:44:00 +0000

    115

--- LeetCode/Chakradhar/115.py
+++ LeetCode/Chakradhar/115.py
@@ -7,7 +7,7 @@ class Solution:
         row1 = [1 for _ in range(c+1)]
         for i in range(r-1, -1, -1):
             row0 = [0 for _ in range(c+1)]
-            for j in range(c-1, -1, -1):
+            for j in range(c-1, i-1, -1):
                 row0[j] = row0[j+1]
                 if s[j] == t[i]:
                     row0[j] += row1[j+1]
