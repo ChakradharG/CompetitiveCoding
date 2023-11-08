7d0195aab5ffca077785d0113069d2fe5418462a
Chakradhar Guntuboina <47364794+ChakradharG@users.noreply.github.com>
2023-11-08 05:49:00 +0000

    135

--- LeetCode/Chakradhar/135.py
+++ LeetCode/Chakradhar/135.py
@@ -6,8 +6,10 @@ class Solution:
             if ratings[i] > ratings[i-1]:
                 candies[i] = candies[i-1] + 1
 
+        total = candies[-1]
         for i in range(len(ratings)-2, -1, -1):
             if ratings[i] > ratings[i+1]:
                 candies[i] = max(candies[i], candies[i+1] + 1)
+            total += candies[i]
 
-        return sum(candies)
+        return total
