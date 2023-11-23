f4982009bcf230286a240080c41171a5cd0988da
Chakradhar Guntuboina <47364794+ChakradharG@users.noreply.github.com>
2023-11-23 20:16:00 +0000

    149

--- LeetCode/Chakradhar/149.py
+++ LeetCode/Chakradhar/149.py
@@ -1,21 +1,27 @@
 class Solution:
     def maxPoints(self, points: List[List[int]]) -> int:
-        maxPts = 1
-        slopes = {}
+        pairs, mx = {}, 1
         for i in range(len(points)-1):
             x1, y1 = points[i]
             for j in range(i+1, len(points)):
                 x2, y2 = points[j]
-                if x1 == x2:
-                    slope = math.inf
-                    intercept = x2
-                else:
-                    slope = (y2 - y1) / (x2 - x1)
-                    intercept = y2 - (slope * x2)
-                if (slope, intercept) not in slopes:
-                    slopes[(slope, intercept)] = set()
-                slopes[(slope, intercept)].add((x1, y1))
-                slopes[(slope, intercept)].add((x2, y2))
-                maxPts = max(maxPts, len(slopes[(slope, intercept)]))
 
-        return maxPts
+                if x1 == x2:
+                    m = math.inf
+                    b = x2
+                else:
+                    m = (y2 - y1) / (x2 - x1)
+                    b = y2 - (m * x2)
+
+                if (m, b) not in pairs:
+                    pairs[(m, b)] = set()
+                pairs[(m, b)].add((x1, y1))
+                pairs[(m, b)].add((x2, y2))
+
+                mx = max(mx, len(pairs[(m, b)]))
+
+        print(pairs)
+        # ans = (1 + math.sqrt(1 + 8 * mx)) / 2
+        ans = mx
+
+        return int(ans)
