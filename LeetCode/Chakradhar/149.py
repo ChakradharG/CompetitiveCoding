c9f36d358feed16bb08219da14b7d66b6fec0bff
Chakradhar Guntuboina <47364794+ChakradharG@users.noreply.github.com>
2023-11-23 21:07:00 +0000

    149

--- LeetCode/Chakradhar/149.py
+++ LeetCode/Chakradhar/149.py
@@ -1,28 +1,21 @@
 class Solution:
     def maxPoints(self, points: List[List[int]]) -> int:
-        pairs, mx = {}, 1
+        maxPts = 1
         for i in range(len(points)-1):
+            slopes = {}
             x1, y1 = points[i]
             for j in range(i+1, len(points)):
                 x2, y2 = points[j]
 
                 if x1 == x2:
                     m = math.inf
-                    b = x2
                 else:
                     m = (y2 - y1) / (x2 - x1)
-                    b = y2 - (m * x2)
 
-                if (m, b) not in pairs:
-                    pairs[(m, b)] = set()
-                pairs[(m, b)].add((x1, y1))
-                pairs[(m, b)].add((x2, y2))
-                print(m, b)
+                if m not in slopes:
+                    slopes[m] = 0
+                slopes[m] += 1
 
-                mx = max(mx, len(pairs[(m, b)]))
+                maxPts = max(maxPts, slopes[m] + 1)
 
-        print(pairs)
-        # ans = (1 + math.sqrt(1 + 8 * mx)) / 2
-        ans = mx
-
-        return int(ans)
+        return maxPts
