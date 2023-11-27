af7236b22589da51ae9f7bccf12d6aa68c74198a
Chakradhar Guntuboina <47364794+ChakradharG@users.noreply.github.com>
2023-11-27 11:02:00 +0000

    935

--- LeetCode/Chakradhar/935.py
+++ LeetCode/Chakradhar/935.py
@@ -2,28 +2,26 @@ class Solution:
     def knightDialer(self, n: int) -> int:
         def neighbors(num):
             if num == 0:
-                return [4, 4]
+                return [3, 3]
             elif num == 1:
-                return [4, 2]
+                return [3, 2]
             elif num == 2:
-                return [3, 1]
+                return [1, 1]
             elif num == 3:
-                return [4, 2]
-            elif num == 4:
-                return [0, 3, 1]
+                return [0, 1, 1]
 
-        row0 = [0 for _ in range(5)]
-        row1 = [1 for _ in range(5)]
+        row0 = [0 for _ in range(4)]
+        row1 = [1 for _ in range(4)]
 
         for i in range(n-1):
-            for j in range(5):
+            for j in range(4):
                 row0[j] = 0
                 for k in neighbors(j):
                     row0[j] += row1[k]
             row0, row1 = row1, row0
 
-        cnt, mod = row1[0], 10**9 + 7
-        for i in range(1, 5):
+        cnt, mod = row1[0] + (2 * row1[1]), 10**9 + 7
+        for i in range(1, 4):
             cnt = (cnt + 2 * row1[i]) % mod
 
         if n == 1:
