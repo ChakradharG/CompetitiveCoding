46c7ce791d5128c87a76c0852e9b08358085c494
Chakradhar Guntuboina <47364794+ChakradharG@users.noreply.github.com>
2023-11-27 10:38:00 +0000

    935

--- LeetCode/Chakradhar/935.py
+++ LeetCode/Chakradhar/935.py
@@ -2,25 +2,25 @@ class Solution:
     def knightDialer(self, n: int) -> int:
         def neighbors(num):
             if num == 0:
-                return [4, 6]
+                return [4, 4]
             elif num == 1:
-                return [6, 8]
+                return [4, 2]
             elif num == 2:
-                return [7, 9]
+                return [3, 1]
             elif num == 3:
-                return [4, 8]
+                return [4, 2]
             elif num == 4:
-                return [0, 3, 9]
-            elif num == 5:
-                return []
-            elif num == 6:
-                return [0, 1, 7]
-            elif num == 7:
-                return [2, 6]
-            elif num == 8:
-                return [1, 3]
-            elif num == 9:
-                return [2, 4]
+                return [0, 3, 1]
+            # elif num == 5:
+            #     return []
+            # elif num == 6:
+            #     return [0, 1, 7]
+            # elif num == 7:
+            #     return [2, 6]
+            # elif num == 8:
+            #     return [1, 3]
+            # elif num == 9:
+            #     return [2, 4]
 
         def dfs(idx, num):
             key = (idx, num)
@@ -39,8 +39,12 @@ class Solution:
 
         dp = {}
         cnt, mod = 0, 10**9 + 7
-        for i in range(10):
+        for i in range(1, 5):
             dfs(0, i)
             cnt = (cnt + dp[(0, i)]) % mod
+        dfs(0, 0)
+        cnt = (2 * cnt + dp[(0, 0)]) % mod
 
+        if n == 1:
+            cnt += 1    # for 5
         return cnt
