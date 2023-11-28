88058aa35d1f851b4b319bcc7d7196437a1784e5
Chakradhar Guntuboina <47364794+ChakradharG@users.noreply.github.com>
2023-11-28 10:30:00 +0000

    2147

--- LeetCode/Chakradhar/2147.py
+++ LeetCode/Chakradhar/2147.py
@@ -1,21 +1,19 @@
 class Solution:
     def numberOfWays(self, corridor: str) -> int:
-        # numS = corridor.count('S')
+        numS = corridor.count('S')
+        if (numS == 0) or (numS % 2 != 0):
+            return 0
 
-        numS = 0
-        cnt, mod = 1, 10**9 + 7
+        cnt = 1
         idx, prev, start = 0, -1, True
         while idx < len(corridor):
             if corridor[idx] != 'S':
                 idx += 1
                 continue
-            numS += 1
             if start and prev != -1:
-                cnt = (cnt * (idx - prev)) % mod
+                cnt *= (idx - prev)
             start = not start
             prev = idx
             idx += 1
 
-        if (numS == 0) or (numS % 2 != 0):
-            return 0
-        return cnt
+        return cnt % (10**9 + 7)
