1af7b4841079d363599d28c3ce305a11f818b15e
Chakradhar Guntuboina <47364794+ChakradharG@users.noreply.github.com>
2023-11-30 15:57:00 +0000

    1611

--- LeetCode/Chakradhar/1611.py
+++ LeetCode/Chakradhar/1611.py
@@ -3,38 +3,36 @@ class Solution:
         if n == 0:
             return 0
 
-        nBits = math.floor(math.log2(n)) + 1
+        bits = []   #LSB -> MSB (order reversed)
+        while n:
+            bits.append(n & 0b01)
+            n >>= 1
 
-        def assignBit(idx, val):
-            nonlocal n
-            if ((n >> idx) & 0b1) == val:
+        def setBit(idx, val):
+            if bits[idx] == val:
                 return 0
 
-            # setting/resetting bit
-            if val:
-                n |= (1 << idx)
-            else:
-                n &= ~(1 << idx)
+            bits[idx] = val
 
             if idx == 0:
                 return 1
 
-            cost = assignBit(idx - 1, 1)
+            cost = setBit(idx - 1, 1)
             if cost != 0:   # meaning it is now of the form x...11x...
                 cnt = 1 + cost + (2**(idx-1) - 1)
             else:
                 cnt = 1 + cost
                 for i in range(idx-2, -1 ,-1):
-                    cost = assignBit(i, 0)
+                    cost = setBit(i, 0)
                     if cost != 0: # meaning it is now of the form x...01x...
                         cnt += cost + (2**i - 1)
                         break
 
             return cnt
 
-        if (math.log2(n) % 1) == 0:
-            ans = 2**nBits - 1
+        if sum(bits) == 1:
+            ans = 2**len(bits) - 1
         else:
-            ans = assignBit(nBits-1, 0) + (2**(nBits-1) - 1)
+            ans = setBit(len(bits) - 1, 0) + 2**(len(bits) - 1) - 1
 
         return ans
