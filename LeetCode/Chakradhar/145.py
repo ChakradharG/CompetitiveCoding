04eb21630f09be1f86ac6b9b5c40413807c69198
Chakradhar Guntuboina <47364794+ChakradharG@users.noreply.github.com>
2023-12-09 17:22:00 +0000

    145

--- LeetCode/Chakradhar/145.py
+++ LeetCode/Chakradhar/145.py
@@ -6,20 +6,15 @@
 #         self.right = right
 class Solution:
     def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-        ans = []
+        ans = deque([])
 
         stack, node = [], root
         while node or stack:
             while node:
+                ans.appendleft(node.val)
                 stack.append(node)
-                stack.append(node.right)
-                node = node.left
+                node = node.right
 
-            node = stack.pop()  # right
-            if node:
-                stack.append(None)
-                continue    # now the right node becomes the "center" and the process is repeated for the subtree. When the entire subtree is processed, we pop the above None so that this node's parent is then printed out (below)
-            ans.append(stack.pop().val)  # center
-            node = None
+            node = stack.pop().left
 
         return ans
