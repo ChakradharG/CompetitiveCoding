6e2dd15526e52331e8b41fb6a678fbab0ec26974
Chakradhar Guntuboina <47364794+ChakradharG@users.noreply.github.com>
2023-12-09 17:15:00 +0000

    145

---
+++ LeetCode/Chakradhar/145.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        ans = []
+
+        stack, node = [], root
+        while node or stack:
+            while node:
+                stack.append(node)
+                stack.append(node.right)
+                node = node.left
+
+            node = stack.pop()  # right
+            if node:
+                stack.append(None)
+                continue
+            ans.append(stack.pop().val)  # center
+            node = None
+
+        return ans
