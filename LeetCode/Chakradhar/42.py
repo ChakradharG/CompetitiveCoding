ab1ddf70b24fc7772e63375fb2037805e5707115
Chakradhar Guntuboina <47364794+ChakradharG@users.noreply.github.com>
2023-12-27 11:22:00 +0000

    42

--- LeetCode/Chakradhar/42.py
+++ LeetCode/Chakradhar/42.py
@@ -3,25 +3,14 @@ class Solution:
         n = len(height)
         water = [math.inf for _ in range(n)]
 
-        lstack, rstack = [], []
+        lMax, rMax = 0, 0
         for i in range(n):
             j = n - i - 1
 
-            while lstack and lstack[-1] <= height[i]:
-                lstack.pop()
-            while rstack and rstack[-1] <= height[j]:
-                rstack.pop()
+            lMax = max(lMax, height[i])
+            water[i] = min(water[i], lMax - height[i])
 
-            if lstack:
-                water[i] = min(water[i], lstack[0] - height[i])
-            else:
-                water[i] = 0
-            if rstack:
-                water[j] = min(water[j], rstack[0] - height[j])
-            else:
-                water[j] = 0
-
-            lstack.append(height[i])
-            rstack.append(height[j])
+            rMax = max(rMax, height[j])
+            water[j] = min(water[j], rMax - height[j])
 
         return sum(water)
