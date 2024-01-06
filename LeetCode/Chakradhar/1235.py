6bff7aa6b129a41ea4486f460e3d46a114106954
Chakradhar Guntuboina <47364794+ChakradharG@users.noreply.github.com>
2024-01-06 03:51:00 +0000

    1235

--- LeetCode/Chakradhar/1235.py
+++ LeetCode/Chakradhar/1235.py
@@ -1,12 +1,10 @@
 class Solution:
     def jobScheduling(self, startTime: List[int], endTime: List[int], profit: List[int]) -> int:
-        jobs = sorted(
-            [(s, e, p) for (s, e, p) in zip(startTime, endTime, profit)]
-        )
+        jobs = sorted(zip(startTime, endTime, profit))
         sortedStarts = [s for s, *_ in jobs]
 
-        row = [0 for _ in range(len(profit)+1)]
-        for i in range(len(profit)-1, -1, -1):
+        row = [0 for _ in range(len(profit) + 1)]
+        for i in range(len(profit) - 1, -1, -1):
             row[i] = max(
                 jobs[i][2] + row[bisect_left(sortedStarts, jobs[i][1], i)],
                 row[i+1]
