1860d2dc72868a3edc7a41763fb85e2f386ba033
Chakradhar Guntuboina <47364794+ChakradharG@users.noreply.github.com>
2024-01-06 03:46:00 +0000

    1235

--- LeetCode/Chakradhar/1235.py
+++ LeetCode/Chakradhar/1235.py
@@ -4,15 +4,11 @@ class Solution:
             [(s, e, p) for (s, e, p) in zip(startTime, endTime, profit)]
         )
         sortedStarts = [s for s, *_ in jobs]
-        next_non_overlap = [
-            bisect.bisect_left(sortedStarts, jobs[i][1], i)
-            for i in range(len(jobs))
-        ]
 
         row = [0 for _ in range(len(profit)+1)]
         for i in range(len(profit)-1, -1, -1):
             row[i] = max(
-                jobs[i][2] + row[next_non_overlap[i]],
+                jobs[i][2] + row[bisect_left(sortedStarts, jobs[i][1], i)],
                 row[i+1]
             )
 
