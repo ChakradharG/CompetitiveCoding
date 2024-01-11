fe3dae55848cc16a277fc8cdec5041bb1d1b0e6f
Chakradhar Guntuboina <47364794+ChakradharG@users.noreply.github.com>
2024-01-11 06:52:00 +0000

    1671

---
+++ LeetCode/Chakradhar/1671.py
@@ -0,0 +1,30 @@
+class Solution:
+    def minimumMountainRemovals(self, nums: List[int]) -> int:
+        n = len(nums)
+        nd = [0 for _ in range(n)]
+
+        LIS = []
+        for i in range(n):
+            index = bisect.bisect_left(LIS, nums[i])
+            if index == len(LIS):
+                LIS.append(nums[i])
+            else:
+                LIS[index] = nums[i]
+            if index == 0:
+                nd[i] = math.inf
+            else:
+                nd[i] = i - index
+
+        LIS = []
+        for i in range(n-1, -1, -1):
+            index = bisect.bisect_left(LIS, nums[i])
+            if index == len(LIS):
+                LIS.append(nums[i])
+            else:
+                LIS[index] = nums[i]
+            if index == 0:
+                nd[i] = math.inf
+            else:
+                nd[i] += (n - i) - (index + 1)
+
+        return min(nd[1:-1])
