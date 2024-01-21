28867b8e1027fa0dab299c238d8af203f6c230e8
Chakradhar Guntuboina <47364794+ChakradharG@users.noreply.github.com>
2024-01-21 01:07:00 +0000

    907

--- LeetCode/Chakradhar/907.py
+++ LeetCode/Chakradhar/907.py
@@ -1,15 +1,14 @@
 class Solution:
     def sumSubarrayMins(self, arr: List[int]) -> int:
         acc, MOD = 0, 10**9 + 7
-        arr = [-math.inf] + arr + [-math.inf]
-        stack = []
+        stack = [(-math.inf, -1)]
 
-        for i, n in enumerate(arr):
-            while stack and n < stack[-1][1]:
-                j, m = stack.pop()
-                left = j - stack[-1][0] if stack else j+1
+        for i, x in enumerate(arr + [-math.inf]):
+            while x < stack[-1][0]:
+                val, j = stack.pop()
+                left = j - stack[-1][1]
                 right = i - j
-                acc = (acc + m * left * right) % MOD
-            stack.append((i, n))
+                acc = (acc + (val * (left * right))) % MOD
+            stack.append((x, i))
 
         return acc
