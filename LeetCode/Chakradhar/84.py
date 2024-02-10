c1ea99120954bdb93951d74972727579146ced6d
Chakradhar Guntuboina <47364794+ChakradharG@users.noreply.github.com>
2024-02-10 08:18:00 +0000

    84

--- LeetCode/Chakradhar/84.py
+++ LeetCode/Chakradhar/84.py
@@ -1,6 +1,6 @@
 class Solution:
     def largestRectangleArea(self, heights: List[int]) -> int:
-        heights.append(-1)
+        heights.append(-1)  # to flush the monotonic increasing stack that we end up with at the end
         stack = []
         maxH = 0
         for i, h in enumerate(heights):
