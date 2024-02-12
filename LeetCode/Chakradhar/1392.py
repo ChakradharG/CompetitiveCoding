16b30ea399317b2157096539ecf80c3794975436
Chakradhar Guntuboina <47364794+ChakradharG@users.noreply.github.com>
2024-02-12 06:20:00 +0000

    1392

---
+++ LeetCode/Chakradhar/1392.py
@@ -0,0 +1,17 @@
+class Solution:
+    def longestPrefix(self, s: str) -> str:
+        l = len(s)
+        LPS = [0 for _ in range(l)]
+        p, c = 0, 1
+        while c < l:
+            if s[p] == s[c]:
+                LPS[c] = p + 1
+                p, c = p + 1, c + 1
+            else:
+                if p == 0:
+                    LPS[c] = 0
+                    c += 1
+                else:
+                    p = LPS[p - 1]
+
+        return s[:LPS[-1]]
