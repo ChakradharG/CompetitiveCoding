2b5ee8772782d5c7ce813939b96a44beb37aa8dd
Chakradhar Guntuboina <47364794+ChakradharG@users.noreply.github.com>
2024-02-25 00:00:00 +0000

    2092

--- LeetCode/Chakradhar/2092.py
+++ LeetCode/Chakradhar/2092.py
@@ -1,19 +1,19 @@
 class Solution:
     def findAllPeople(self, n: int, meetings: List[List[int]], firstPerson: int) -> List[int]:
-        adj = {i: [math.inf, []] for i in range(n)} # [t, nei]
+        adj = {i: [] for i in range(n)}
         for (x, y, t) in meetings:
-            adj[x][1].append((y, t))
-            adj[y][1].append((x, t))
-        adj[0][1].append((firstPerson, 0))
+            adj[x].append((y, t))
+            adj[y].append((x, t))
+        adj[0].append((firstPerson, 0))
 
         q = deque([0])
-        adj[0][0] = 0
+        vis = {0: 0}
         while q:
             x = q.popleft()
-            for (y, t) in adj[x][1]:
-                if t >= adj[x][0]: # x has secret at the time of meeting y
-                    if t < adj[y][0]:  # found a way to get secret to y (faster)
-                        adj[y][0] = t
+            for (y, t) in adj[x]:
+                if t >= vis[x]: # x has secret at the time of meeting y
+                    if y not in vis or vis[y] > t:  # found a way to get secret to y (faster)
+                        vis[y] = t
                         q.append(y)
 
-        return [k for k, v in adj.items() if v[0] != math.inf]
+        return vis.keys()
