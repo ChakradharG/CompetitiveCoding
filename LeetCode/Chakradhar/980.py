08a470b1c37fb0af9f84f30f772936c8e5eb3152
Chakradhar Guntuboina <47364794+ChakradharG@users.noreply.github.com>
2024-03-01 18:49:00 +0000

    980

--- LeetCode/Chakradhar/980.py
+++ LeetCode/Chakradhar/980.py
@@ -1,28 +1,26 @@
 class Solution:
     def uniquePathsIII(self, grid: List[List[int]]) -> int:
-        m, n = len(grid), len(grid[0])
-        vis = set()
-
-        def dfs(i, j):
-            if not (0 <= i < m and 0 <= j < n):
-                return 0
-            
+        def dfs(i, j, free, vis):
             if grid[i][j] == -1:
                 return 0
             elif grid[i][j] == 2:
-                return (len(vis)-1) == free
+                return (free == 0)
 
-            vis.add((i, j))
-            top = dfs(i-1, j) if (i-1, j) not in vis else 0
-            left = dfs(i, j-1) if (i, j-1) not in vis else 0
-            right = dfs(i, j+1) if (i, j+1) not in vis else 0
-            bottom = dfs(i+1, j) if (i+1, j) not in vis else 0
-            vis.remove((i, j))
+            ways = 0
+            for (ni, nj) in [(i-1, j), (i, j-1), (i, j+1), (i+1, j)]:
+                if 0 <= ni < m and 0 <= nj < n:
+                    bitpos = n*i + j
+                    if not ((vis >> bitpos) & 0b1):
+                        ways += dfs(
+                            ni, nj, free-1, 
+                            vis | (0b1 << bitpos)
+                        )
 
-            return top + left + right + bottom
+            return ways
 
+        m, n = len(grid), len(grid[0])
         start = (-1, -1)
-        free = 0
+        free = 1    # counting start as free to simplify code in `dfs`
         for i in range(m):
             for j in range(n):
                 if grid[i][j] == 1:
@@ -30,4 +28,4 @@ class Solution:
                 elif grid[i][j] == 0:
                     free += 1
 
-        return dfs(*start)
+        return dfs(*start, free, 0)
