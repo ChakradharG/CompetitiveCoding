f3577a8129bbb9a9321995311737dd2b4b41a1a8
Chakradhar Guntuboina <47364794+ChakradharG@users.noreply.github.com>
2024-03-01 18:27:00 +0000

    980

---
+++ LeetCode/Chakradhar/980.py
@@ -0,0 +1,34 @@
+class Solution:
+    def uniquePathsIII(self, grid: List[List[int]]) -> int:
+        m, n = len(grid), len(grid[0])
+        vis = set()
+
+        def dfs(i, j):
+            if not (0 <= i < m and 0 <= j < n):
+                return 0
+            
+            if grid[i][j] == -1:
+                return 0
+            elif grid[i][j] == 2:
+                return (len(vis)-1) == free
+
+            vis.add((i, j))
+            top = dfs(i-1, j) if (i-1, j) not in vis else 0
+            left = dfs(i, j-1) if (i, j-1) not in vis else 0
+            right = dfs(i, j+1) if (i, j+1) not in vis else 0
+            bottom = dfs(i+1, j) if (i+1, j) not in vis else 0
+            vis.remove((i, j))
+
+            return top + left + right + bottom
+
+        start = (-1, -1)
+        obs = 0
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 1:
+                    start = (i, j)
+                elif grid[i][j] == -1:
+                    obs += 1
+        free = m*n - 2 - obs
+
+        return dfs(*start)
