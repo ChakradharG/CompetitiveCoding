1f8f5491b477509bb96e9a352353ded7ae443889
Chakradhar Guntuboina <47364794+ChakradharG@users.noreply.github.com>
2024-03-02 21:39:00 +0000

    1840

--- LeetCode/Chakradhar/1840.py
+++ LeetCode/Chakradhar/1840.py
@@ -3,8 +3,9 @@ class Solution:
         rests = sorted(
             [[0, 0], [n-1, n-1]] + 
             [[i-1, h] for (i, h) in restrictions]
-        )
+        )   # adding default cases to rests and sorting by id
 
+        # 1st pass (l to r)
         for i in range(1, len(rests)):
             diff = rests[i][0] - rests[i-1][0]
             rests[i][1] = min(
@@ -13,6 +14,7 @@ class Solution:
             )
 
         ans = rests[-1][1]
+        # 2nd pass (r to l)
         for i in range(len(rests)-2, -1, -1):
             diff = rests[i+1][0] - rests[i][0]
             rests[i][1] = min(
@@ -21,8 +23,10 @@ class Solution:
             )
             hdiff = abs(rests[i+1][1] - rests[i][1])
             if (diff - hdiff) > 1:
-                d = diff - hdiff
-                ans = max(ans, max(rests[i][1], rests[i+1][1]) + d//2)
+                # mountain b/w i and i+1 (i.e., peak somewhere in between)
+                d = (diff - hdiff) // 2
+                peak = max(rests[i][1], rests[i+1][1]) + d
+                ans = max(ans, peak)
             else:
                 ans = max(ans, rests[i][1])
 
