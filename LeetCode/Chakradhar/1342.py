56c67ba36f24037c957f91c289d59b770ab45c69
Chakradhar Guntuboina <47364794+ChakradharG@users.noreply.github.com>
2024-03-03 05:15:00 +0000

    1342

--- LeetCode/Chakradhar/1342.py
+++ LeetCode/Chakradhar/1342.py
@@ -1,11 +1,17 @@
 class Solution:
     def numberOfSteps(self, num: int) -> int:
-        cnt = 0
-        while num > 0:
-            if num % 2:
-                num -= 1
-            else:
-                num //= 2
-            cnt += 1
+        if num == 0:
+            return 0
 
-        return cnt
+        zer, one = 0, 0
+        # to get rid of zero at LSB is 1 operation: division
+        # to get rid of one at LSB is 2 operations: sub + div
+        #   (except the last most significant one, which only need 1 op)
+        while num:
+            if num & 0b1:
+                one += 1
+            else:
+                zer += 1
+            num >>= 1
+
+        return zer + (2*one - 1)
