5778ed082f5ff566192432b9df7d2b88de1bb0e5
Chakradhar Guntuboina <47364794+ChakradharG@users.noreply.github.com>
2024-03-03 03:51:00 +0000

    3072

---
+++ LeetCode/Chakradhar/3072.py
@@ -0,0 +1,38 @@
+from sortedcontainers import SortedList as s
+# from bisect import bisect as b
+
+class Solution:
+    def resultArray(self, nums: List[int]) -> List[int]:
+        # def greaterCount(l, v):
+        #     cnt = 0
+        #     for x in l:
+        #         if x > v:
+        #             cnt += 1
+        #     return cnt
+
+        sarr1, sarr2 = s([nums[0]]), s([nums[1]])
+        arr1, arr2 = [nums[0]], [nums[1]]
+        l1, l2 = 1, 1
+        for i in range(2, len(nums)):
+            gc1 = l1 - sarr1.bisect_right(nums[i])
+            gc2 = l2 - sarr2.bisect_right(nums[i])
+            if gc1 > gc2:
+                arr1.append(nums[i])
+                sarr1.add(nums[i])
+                l1 += 1
+            elif gc1 < gc2:
+                arr2.append(nums[i])
+                sarr2.add(nums[i])
+                l2 += 1
+            else:
+                if l1 > l2:
+                    arr2.append(nums[i])
+                    sarr2.add(nums[i])
+                    l2 += 1
+                else:
+                    arr1.append(nums[i])
+                    sarr1.add(nums[i])
+                    l1 += 1
+
+        return arr1 + arr2                    
+        
