9aa4555abe481ee049db645efa82b7414d92cb88
Chakradhar Guntuboina <47364794+ChakradharG@users.noreply.github.com>
2024-03-03 08:03:00 +0000

    1622

--- LeetCode/Chakradhar/1622.py
+++ LeetCode/Chakradhar/1622.py
@@ -9,20 +9,10 @@ class Fancy:
 
     def addAll(self, inc: int) -> None:
         self.a += inc
-        # if len(self.l) == 0:
-        #     return
-        # while len(self.ops) > self.l[-1][1] and self.ops[-1][0] == '+':
-        #     inc += self.ops.pop()[1]
-        # self.ops.append(['+', inc % MOD])
 
     def multAll(self, m: int) -> None:
         self.m *= m
         self.a *= m
-        # if len(self.l) == 0:
-        #     return
-        # while len(self.ops) > self.l[-1][1] and self.ops[-1][0] == '*':
-        #     m *= self.ops.pop()[1]
-        # self.ops.append(['*', m % MOD])
 
     def getIndex(self, idx: int) -> int:
         if idx >= len(self.l):
@@ -31,17 +21,6 @@ class Fancy:
         alpha = self.m // alpha
         beta = self.a - (alpha*beta)
         return (alpha*x + beta) % MOD
-        # x, i = self.l[idx]
-        # while i < len(self.ops):
-        #     op, y = self.ops[i]
-        #     if op == '+':
-        #         x += y
-        #     else:
-        #         x = (x * y) % MOD
-        #     i += 1
-        # x %= MOD
-        # self.l[idx] = [x, i]
-        # return x
 
 MOD = 10**9 + 7
 # Your Fancy object will be instantiated and called as such:
