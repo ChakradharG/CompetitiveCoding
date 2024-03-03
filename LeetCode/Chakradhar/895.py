f450b9ce4f0c95c582c5ca1bcf5e0b3d83ba00e5
Chakradhar Guntuboina <47364794+ChakradharG@users.noreply.github.com>
2024-03-03 08:20:00 +0000

    895

---
+++ LeetCode/Chakradhar/895.py
@@ -0,0 +1,27 @@
+class FreqStack:
+
+    def __init__(self):
+        self.h = []
+        self.c = {}
+        self.counter = 0
+
+    def push(self, val: int) -> None:
+        if val not in self.c:
+            self.c[val] = [0, 0]
+        self.c[val][0] += 1
+        self.c[val][1] = self.counter
+        self.counter += 1
+        heapq.heappush(self.h, (-self.c[val][0], -self.c[val][1], val))
+
+    def pop(self) -> int:
+        # while self.h[0][0] != -self.c[self.h[0][2]][0]:
+        #     heapq.heappop(self.h)
+        *_, val = heapq.heappop(self.h)
+        self.c[val][0] -= 1
+        return val
+
+
+# Your FreqStack object will be instantiated and called as such:
+# obj = FreqStack()
+# obj.push(val)
+# param_2 = obj.pop()
