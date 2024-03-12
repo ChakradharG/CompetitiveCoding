b08181bb5b0779858cdeb9ad8208253215a6e5eb
Chakradhar Guntuboina <47364794+ChakradharG@users.noreply.github.com>
2024-03-12 07:31:00 +0000

    2751

--- LeetCode/Chakradhar/2751.py
+++ LeetCode/Chakradhar/2751.py
@@ -9,13 +9,13 @@ class Solution:
             else:   # will collide with ones to its left and moving right
                 while stack and stack[-1][2] == 'R':
                     p2, h2, d2, i2 = stack.pop()
-                    if h > h2:
+                    if h > h2:  # current robot survives
                         h -= 1
-                    else:
-                        if h < h2:
+                    else:   # both perish or prev survives
+                        if h < h2:  # prev robot survives
                             stack.append((p2, h2-1, d2, i2))
                         break
-                else:
+                else:   # cur survives and no more collisions to its left
                     stack.append((p, h, d, i))
 
         return list(map(lambda x: x[1], sorted(stack, key=lambda x: x[3]))) # re-sorting in original order
