4672e368ae270aa43ccda5e9d89ee158805eff02
Chakradhar Guntuboina <47364794+ChakradharG@users.noreply.github.com>
2024-03-15 19:08:00 +0000

    403

---
+++ LeetCode/Chakradhar/403.py
@@ -0,0 +1,18 @@
+class Solution:
+    def canCross(self, stones: List[int]) -> bool:
+        def dfs(i, k):
+            if i == len(stones)-1:
+               return True
+
+            key = (i, k)
+            if key not in memo:
+                memo[key] = False
+                jump = k - 1
+                for jump in [(k - 1), k, (k + 1)]:
+                    if jump > 0 and (stones[i] + jump) in rev_map:
+                        memo[key] |= dfs(rev_map[stones[i] + jump], jump)
+            return memo[key]
+
+        memo = {}
+        rev_map = {stones[i]: i for i in range(len(stones))}
+        return dfs(0, 0)
