9cc1eadee9ebf71db6fc7dae8fa8dd251b160b58
Chakradhar Guntuboina <47364794+ChakradharG@users.noreply.github.com>
2024-03-29 19:11:00 +0000

    2302

--- LeetCode/Chakradhar/2302.py
+++ LeetCode/Chakradhar/2302.py
@@ -2,12 +2,12 @@ class Solution:
     def countSubarrays(self, nums: List[int], k: int) -> int:
         n = len(nums)
         prefix = [0 for _ in range(n+1)]  # +1 for offset
-        left, cnt = 0, 0
+        l, cnt = 0, 0
 
         for i in range(n):
             prefix[i+1] = prefix[i] + nums[i]
 
-            l, r = left, i+1
+            r = i+1
             while l < r:
                 m = (l + r) // 2
                 score = (prefix[i+1] - prefix[m]) * ((i+1) - m)
@@ -15,8 +15,7 @@ class Solution:
                     l = m + 1
                 else:
                     r = m
-            left = l
 
-            cnt += ((i+1) - left)
+            cnt += ((i+1) - l)
 
         return cnt
