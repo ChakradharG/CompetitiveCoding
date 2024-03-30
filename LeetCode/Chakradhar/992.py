d9a93c5ddc26440f5c7fda2ff9983bd86b8aa5f7
Chakradhar Guntuboina <47364794+ChakradharG@users.noreply.github.com>
2024-03-30 21:00:00 +0000

    992

--- LeetCode/Chakradhar/992.py
+++ LeetCode/Chakradhar/992.py
@@ -2,29 +2,26 @@ class Solution:
     def subarraysWithKDistinct(self, nums: List[int], k: int) -> int:
         cur, cnt = 0, 0
         d = defaultdict(int)
-        l, r = 0, 0
+        j, l, r = 0, 0, 0
 
         while r < len(nums):
-            if d[nums[r]] == 0:
-                cur += 1
             d[nums[r]] += 1
+            if d[nums[r]] == 1:
+                cur += 1
 
             while cur > k:
                 d[nums[l]] -= 1
                 if d[nums[l]] == 0:
                     cur -= 1
                 l += 1
+                j = l
+
+            while d[nums[l]] > 1:
+                d[nums[l]] -= 1
+                l += 1
+
             if cur == k:
-                j = r + 1
-                while j < len(nums) and d[nums[j]] != 0:
-                    j += 1
-                i = l
-                while cur == k:
-                    d[nums[l]] -= 1
-                    if d[nums[l]] == 0:
-                        cur -= 1
-                    l += 1
-                cnt += (l - i) * (j - r)
+                cnt += (l - j + 1)
 
             r += 1
 
