7530a0e828fe0d60d91ba4312ad6168302221927
Chakradhar Guntuboina <47364794+ChakradharG@users.noreply.github.com>
2024-04-26 05:49:00 +0000

    2370

--- LeetCode/Chakradhar/2370.py
+++ LeetCode/Chakradhar/2370.py
@@ -1,20 +1,19 @@
 class Solution:
     def longestIdealString(self, s: str, k: int) -> int:
         n = len(s)
-        s = list(map(lambda x: ord(x) - 96, s))
+        s = list(map(lambda x: ord(x) - 97, s))
 
-        row = [0 for _ in range(27)]    # idx 0 -> blank
+        row = [0 for _ in range(26)]
 
         for i in range(n-1, -1, -1):
             pivot = s[i]
-            l = max(1, pivot - k)
-            r = min(26, pivot + k)
+            l = max(0, pivot - k)
+            r = min(25, pivot + k)
 
             indices = set(range(l, r + 1))
-            indices.add(0)
             indices.remove(pivot)
 
-            for j in indices:   # loop over ideal indices only (and blank)
+            for j in indices:   # loop over ideal indices only
                 row[j] = max(
                     row[j],         # skip
                     row[pivot] + 1  # include
