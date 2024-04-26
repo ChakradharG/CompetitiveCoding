bb185902c7abb13476458bb19db937c93fd00bc4
Chakradhar Guntuboina <47364794+ChakradharG@users.noreply.github.com>
2024-04-26 06:47:00 +0000

    2370

--- LeetCode/Chakradhar/2370.py
+++ LeetCode/Chakradhar/2370.py
@@ -1,12 +1,11 @@
 class Solution:
     def longestIdealString(self, s: str, k: int) -> int:
         n = len(s)
-        s = list(map(lambda x: ord(x) - 97, s))
 
         row = [0 for _ in range(26)]
 
         for i in range(n-1, -1, -1):
-            pivot = s[i]
+            pivot = ord(s[i]) - 97
             l = max(0, pivot - k)
             r = min(25, pivot + k)
 
