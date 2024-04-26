b8e78cc7df5fe3dca7cdf73237b223ea15662843
Chakradhar Guntuboina <47364794+ChakradharG@users.noreply.github.com>
2024-04-26 10:08:00 +0000

    1289

--- LeetCode/Chakradhar/1289.py
+++ LeetCode/Chakradhar/1289.py
@@ -1,16 +1,28 @@
 class Solution:
     def minFallingPathSum(self, grid: List[List[int]]) -> int:
         n = len(grid)
-        row0 = [0 for _ in range(n)]
-        row1 = grid[-1]
+        if n == 1:
+            return grid[0][0]
 
-        for i in range(n-2, -1, -1):
+        row0 = [0 for _ in range(n)]
+        row1 = [0 for _ in range(n)]
+
+        for i in range(n-1, -1, -1):
+            # finding 2 mins
+            m1, k, m2 = row1[0], 0, math.inf
+            for j in range(1, n):
+                if row1[j] < m1:
+                    m2 = m1
+                    m1, k = row1[j], j
+                elif row1[j] < m2:
+                    m2 = row1[j]
+
             for j in range(n):
-                row0[j] = math.inf
-                for k in range(n):
-                    if k != j:
-                        row0[j] = min(row0[j], row1[k])
-                row0[j] += grid[i][j]
+                if j == k:
+                    row0[j] = grid[i][j] + m2
+                else:
+                    row0[j] = grid[i][j] + m1
+
             row0, row1 = row1, row0
 
         return min(row1)
