c7002e79191d6877a1aad5a6d2dfb7e2e57ca0b3
Chakradhar Guntuboina <47364794+ChakradharG@users.noreply.github.com>
2024-05-06 16:01:00 +0000

    1526

--- LeetCode/Chakradhar/1526.py
+++ LeetCode/Chakradhar/1526.py
@@ -2,7 +2,6 @@ class Solution:
     def minNumberOperations(self, target: List[int]) -> int:
         cnt = 0
         stack = []
-        target.append(0)    # will clear the monotonic stack
         for num in target:
             if stack and stack[-1] > num:
                 # [1,3,4] only needs 4 steps
@@ -11,4 +10,4 @@ class Solution:
                 stack.pop()
             stack.append(num)
 
-        return cnt
+        return cnt + stack[-1]
