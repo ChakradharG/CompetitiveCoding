3de1ed25db7783cf0e68d2c200bf50e802b49a59
Chakradhar Guntuboina <47364794+ChakradharG@users.noreply.github.com>
2024-05-13 13:49:00 +0000

    861

--- LeetCode/Chakradhar/861.py
+++ LeetCode/Chakradhar/861.py
@@ -2,24 +2,22 @@ class Solution:
     def matrixScore(self, grid: List[List[int]]) -> int:
         m, n = len(grid), len(grid[0])
 
-        for i in range(m):
-            if grid[i][0] == 0:
-                # flip
-                for j in range(n):
-                    grid[i][j] = int(not grid[i][j])
-
-        for j in range(n):
+        ans = 0
+        pos = 1
+        for j in range(n-1, -1, -1):
             z = 0
             for i in range(m):
-                if grid[i][j] == 0:
+                if grid[i][0] != grid[i][j]:
+                    # implicitly storing whether row is flipped in 1st cell
+                    # if 1st cell is 0, cur cell will be flipped
+                    # 1st cell | cur cell | effective cur cell
+                    #   0      |    0     |     1
+                    #   0      |    1     |     0
+                    #   1      |    0     |     0
+                    #   1      |    1     |     1
                     z += 1
-            if z > (m/2):
-                # flip
-                for i in range(m):
-                    grid[i][j] = int(not grid[i][j])
-
-        ans = 0
-        for i in range(m):
-            ans += int(''.join(map(str, grid[i])), base=2)
+            z = max(z, m - z)   # flip column or not
+            ans += z * pos
+            pos <<= 1
 
         return ans
