e1fe76864eb48cfefc930cbbc3def0491a82caaf
Chakradhar Guntuboina <47364794+ChakradharG@users.noreply.github.com>
2024-05-19 22:48:00 +0000

    3068

---
+++ LeetCode/Chakradhar/3068.py
@@ -0,0 +1,22 @@
+class Solution:
+    def maximumValueSum(self, nums: List[int], k: int, edges: List[List[int]]) -> int:
+        # a -- b -- c
+        # if optimal solution needs a and c to be XOR'd
+        # XOR a -- b, then XOR b -- c
+        # i.e., if 2 nodes benefit from XOR, they don't need to share a direct edge
+
+        cnt, total, minD = 0, 0, math.inf
+        for num in nums:
+            xNum = num ^ k
+            if xNum >= num:
+                cnt += 1
+                total += xNum
+            else:
+                total += num
+            minD = min(minD, abs(xNum - num))
+
+        if cnt % 2 == 0:
+            # can divide into pairs and XOR all
+            return total
+        else:
+            return total - minD
