ec68127de84c6ca8b29ebfbdc7af2ab46819fe6b
Chakradhar Guntuboina <47364794+ChakradharG@users.noreply.github.com>
2024-05-24 23:12:00 +0000

    3097

--- LeetCode/Chakradhar/3097.py
+++ LeetCode/Chakradhar/3097.py
@@ -1,38 +1,24 @@
 class Solution:
     def minimumSubarrayLength(self, nums: List[int], k: int) -> int:
-        def forwardOr(num):
-            num = ('0' * (32 - len(num))) + num
-            cur = 0
-            for i in range(32):
-                cur <<= 1
-                bits[i] += (num[i] == '1')
-                cur += (bits[i] > 0)
-            return cur
-
-        def backwardOr(num):
-            num = ('0' * (32 - len(num))) + num
-            cur = 0
-            for i in range(32):
-                cur <<= 1
-                bits[i] -= (num[i] == '1')
-                cur += (bits[i] > 0)
-            return cur
-
-        bits = [0 for _ in range(32)]
-
-        l, r = 0, 0
+        r = 0
         cur, ans = 0, math.inf
 
         while r < len(nums):
-            cur = forwardOr(bin(nums[r])[2:])
+            print('r', r)
+            cur |= nums[r]
 
-            while l <= r and cur >= k:
-                ans = min(ans, r - l + 1)
-                cur = backwardOr(bin(nums[l])[2:])
-                l += 1
+            if cur >= k:
+                cur = 0
+                l = r
+                while cur < k:
+                    print('l', l)
+                    cur |= nums[l]
+                    l -= 1
+                ans = min(ans, r - l)
+                r, cur = l + 1, 0
 
             r += 1
 
         if ans == math.inf:
             return -1
-        return ans
+        return max(ans, 1)  # k = 0 edge case
