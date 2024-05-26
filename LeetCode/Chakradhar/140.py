f9f8fd461e0cd4035e8d9d7f0bcc26074fd8f27d
Chakradhar Guntuboina <47364794+ChakradharG@users.noreply.github.com>
2024-05-26 18:26:00 +0000

    140

---
+++ LeetCode/Chakradhar/140.py
@@ -0,0 +1,21 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
+        def dfs(i, cur):
+            if i == len(s):
+                if cur == '' or cur in wordDict:
+                    return [cur]
+                else:
+                    return [] # can't break
+
+            key = (i, cur)
+            if key not in memo:
+                memo[key] = dfs(i+1, cur + s[i])
+                if cur in wordDict:
+                    for sub in dfs(i, ''):
+                        memo[key].append(cur + ' ' + sub)
+            return memo[key]
+
+        wordDict = set(wordDict)
+        memo = {}   # (index, current unbroken string) -> [possible strings]
+
+        return dfs(0, '')
