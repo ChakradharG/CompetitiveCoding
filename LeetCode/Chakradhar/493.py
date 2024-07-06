6f0dfef2b589c832782eb1fe0023be65b07b5d46
Chakradhar Guntuboina <47364794+ChakradharG@users.noreply.github.com>
2024-07-06 04:23:00 +0000

    493

--- LeetCode/Chakradhar/493.py
+++ LeetCode/Chakradhar/493.py
@@ -1,12 +1,46 @@
-from sortedcontainers import SortedList
-
 class Solution:
     def reversePairs(self, nums: List[int]) -> int:
-        sl = SortedList()
-        cnt = 0
-        for num in nums:
-            i = sl.bisect_right(2 * num)
-            cnt += (len(sl) - i)
-            sl.add(num)
+        def merge(a, b):
+            m, n = len(a), len(b)
+            i, j = 0, 0
+            cnt = 0
+            while (i < m) and (j < n):
+                if a[i] > 2 * b[j]:
+                    cnt += (m - i)
+                    j += 1
+                else:
+                    i += 1
 
-        return cnt
+            i, j = 0, 0
+            c = []
+            while (i < m) and (j < n):
+                if a[i] <= b[j]:
+                    c.append(a[i])
+                    i += 1
+                else:
+                    c.append(b[j])
+                    j += 1
+
+            while i < m:
+                c.append(a[i])
+                i += 1
+            while j < n:
+                c.append(b[j])
+                j += 1
+
+            return c, cnt
+
+        def divide(arr):
+            if len(arr) < 2:
+                return arr, 0
+
+            beg, end = 0, len(arr)
+            m = (beg + end) // 2
+
+            lArr, lCnt = divide(arr[:m])
+            rArr, rCnt = divide(arr[m:])
+            arr, cnt = merge(lArr, rArr)
+
+            return arr, (lCnt + cnt + rCnt)
+
+        return divide(nums)[1]
