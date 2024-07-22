629400584c647a0264a6a2954bc3b9330e0db44f
Chakradhar Guntuboina <47364794+ChakradharG@users.noreply.github.com>
2024-07-22 17:54:00 +0000

    1458

--- LeetCode/Chakradhar/1458.py
+++ LeetCode/Chakradhar/1458.py
@@ -1,22 +1,19 @@
 class Solution:
     def maxDotProduct(self, nums1: List[int], nums2: List[int]) -> int:
-        def dfs(i, j):
-            if (i == n1) or (j == n2):
-                return -math.inf
+        m, n = len(nums1), len(nums2)
+        row0 = [-math.inf for _ in range(n+1)]
+        row1 = [-math.inf for _ in range(n+1)]
 
-            key = (i, j)
-            if key not in memo:
+        for i in reversed(range(m)):
+            for j in reversed(range(n)):
                 cur = nums1[i] * nums2[j]
-                memo[key] = max(
-                    cur + dfs(i+1, j+1),    # use i j and go further
-                    cur,                    # use i j and stop here
-                    dfs(i+1, j+1),          # skip both
-                    dfs(i+1, j),            # skip i
-                    dfs(i, j+1)             # skip j
+                row0[j] = max(
+                    cur + row1[j+1],    # use i j and go further
+                    cur,                # use i j and stop here
+                    row1[j+1],          # skip both
+                    row1[j],            # skip i
+                    row0[j+1]           # skip j
                 )
+            row0, row1 = row1, row0
 
-            return memo[key]
-
-        n1, n2 = len(nums1), len(nums2)
-        memo = {}
-        return dfs(0, 0)
+        return row1[0]
