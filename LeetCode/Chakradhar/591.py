acbb86b034eaedb0d662570d78516597833f80ee
Chakradhar Guntuboina <47364794+ChakradharG@users.noreply.github.com>
2024-10-09 04:45:00 +0000

    591

--- LeetCode/Chakradhar/591.py
+++ LeetCode/Chakradhar/591.py
@@ -10,38 +10,30 @@ class Solution:
                 r += 1
                 continue
             if (r == n - 1) or code[r+1] == '>':    # unmatched `<` or TAG_NAME is empty
-                print(0)
                 return False
             if code[r+1] == '/':    # end tag
                 if len(stack) == 0:  # unmatched end tag
-                    print(1)
                     return False
                 pl, pr = stack.pop()
-                tag_name = code[pl: pr]
                 l, r = r + 2, r + 2
-                while (r < n) and (code[r] != '>') and (code[r] == tag_name[r - l]):
+                while (r < n) and (code[r] != '>') and (code[r] == code[r - l + pl]):
                     r += 1
-                if (r - l) != len(tag_name):
-                    print(2)
+                if (r - l) != (pr - pl):
                     return False
                 if (r == n-1) and (pl != 1):
-                    print(10)
                     return False
                 l = r + 1
             elif code[r+1] == '!':  # CDATA
                 l, r = r + 2, r + 2
                 if (n - r) < 10:    # invalid CDATA
-                    print(3)
                     return False
                 while ((r - l) < 7) and (code[r] == cdata[r - l]):
                     r += 1
                 if (r - l) < 7:
-                    print(4)
                     return False
                 while r < (n - 2) and not (code[r] == ']' and code[r+1] == ']' and code[r+2] == '>'):
                     r += 1
                 if r == (n - 2):
-                    print(5)
                     return False
                 l = r + 1
             else:   # start tag
@@ -50,7 +42,6 @@ class Solution:
                 while (r < n) and code[r] != '>' and (65 <= ord(code[r]) <= 90) and ((r - l) < 9):
                     r += 1
                 if r == n or code[r] != '>':
-                    print(6)
                     return False
                 stack.append((l, r))
                 l = r + 1
@@ -59,5 +50,4 @@ class Solution:
         if start_tag and (len(stack) == 0) and (l >= r):
             return True
         else:
-            print(7)
             return False
