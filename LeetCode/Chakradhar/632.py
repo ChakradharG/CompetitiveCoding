86c11fd16fb34da615bc8dc321e8c6395c9be2ad
Chakradhar Guntuboina <47364794+ChakradharG@users.noreply.github.com>
2024-10-14 20:43:00 +0000

    632

--- LeetCode/Chakradhar/632.py
+++ LeetCode/Chakradhar/632.py
@@ -1,35 +1,25 @@
 class Solution:
     def smallestRange(self, nums: List[List[int]]) -> List[int]:
-        merged = []
+        k = len(nums)
+        ptrs, h = [], []
+        mx = -math.inf
+        for i in range(k):
+            ptrs.append(1)
+            h.append((nums[i][0], i))
+            mx = max(mx, nums[i][0])
+        heapify(h)
+        mn = h[0][0]
 
-        # Merge all lists with their list index
-        for i in range(len(nums)):
-            for num in nums[i]:
-                merged.append((num, i))
+        ans = [mn, mx]
+        while True:
+            x, i = heappop(h)
+            if ptrs[i] == len(nums[i]):
+                break
+            y = nums[i][ptrs[i]]
+            heappush(h, (y, i))
+            ptrs[i] += 1
+            mn, mx = h[0][0], max(mx, y)
+            if (mx - mn) < (ans[1] - ans[0]):
+                ans = [mn, mx]
 
-        # Sort the merged list
-        merged.sort()
-
-        # Two pointers to track the smallest range
-        freq = defaultdict(int)
-        left, count = 0, 0
-        range_start, range_end = 0, float("inf")
-
-        for right in range(len(merged)):
-            freq[merged[right][1]] += 1
-            if freq[merged[right][1]] == 1:
-                count += 1
-
-            # When all lists are represented, try to shrink the window
-            while count == len(nums):
-                cur_range = merged[right][0] - merged[left][0]
-                if cur_range < range_end - range_start:
-                    range_start = merged[left][0]
-                    range_end = merged[right][0]
-
-                freq[merged[left][1]] -= 1
-                if freq[merged[left][1]] == 0:
-                    count -= 1
-                left += 1
-
-        return [range_start, range_end]
+        return ans
