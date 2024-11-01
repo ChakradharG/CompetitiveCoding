28b14f336190b7d9d114b3150d4e5a45932e45e1
Chakradhar Guntuboina <47364794+ChakradharG@users.noreply.github.com>
2024-11-01 05:19:00 +0000

    2463

--- LeetCode/Chakradhar/2463.py
+++ LeetCode/Chakradhar/2463.py
@@ -6,15 +6,15 @@ class Solution:
         for (p, l) in factory:
             factories.extend([p]*l)
         m, n = len(robot), len(factories)
-        print(robot, factories)
+        k = n + 1 - m
 
-        row0 = [0 for _ in range(n+1)]
-        row1 = [0 for _ in range(n+1)]
+        row0 = [0 for _ in range(k+1)]
+        row1 = [0 for _ in range(k+1)]
 
         for i in reversed(range(m)):
             row0[-1] = math.inf
-            for j in reversed(range(n)):
-                row0[j] = min(row0[j+1], abs(robot[i] - factories[j]) + row1[j+1])
+            for j in reversed(range(k)):
+                row0[j] = min(row0[j+1], abs(robot[i] - factories[j+i]) + row1[j])
             row0, row1 = row1, row0
 
         return row1[0]
