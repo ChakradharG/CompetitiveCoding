7eac29453158791073c7f4755834d4302990faf8
Chakradhar Guntuboina <47364794+ChakradharG@users.noreply.github.com>
2024-11-28 17:53:00 +0000

    2290

---
+++ LeetCode/Chakradhar/2290.py
@@ -0,0 +1,19 @@
+class Solution:
+    def minimumObstacles(self, grid: List[List[int]]) -> int:
+        m, n = len(grid), len(grid[0])
+        h = [(0, 0, 0)]
+        d = [[math.inf for j in range(n)] for i in range(m)]
+        d[0][0] = 0
+
+        while h:
+            w, i, j = heappop(h)
+            if (i == m-1) and (j == n-1):
+                break
+            for ni, nj in [(i-1, j), (i+1, j), (i, j-1), (i, j+1)]:
+                if (0 <= ni < m) and (0 <= nj < n):
+                    nw = w + grid[ni][nj]
+                    if nw < d[ni][nj]:
+                        d[ni][nj] = nw
+                        heappush(h, (nw, ni, nj))
+
+        return d[m-1][n-1]
