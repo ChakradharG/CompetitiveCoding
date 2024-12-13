b3f2a43f5e1947389a20b6ed291b8aaeb5eb3de2
Chakradhar Guntuboina <47364794+ChakradharG@users.noreply.github.com>
2024-12-13 02:59:00 +0000

    2593

--- LeetCode/Chakradhar/2593.py
+++ LeetCode/Chakradhar/2593.py
@@ -1,14 +1,15 @@
 class Solution:
     def findScore(self, nums: List[int]) -> int:
         n = len(nums)
-        nums.append(math.inf)   # simplify checking boundary condition
+        nums.append(math.inf)
         score = 0
         l, r = 0, 0
         while r < n:
-            if nums[r] <= nums[r+1]:
+            if nums[r] > nums[r+1]:
+                r += 1
+            else:
                 for i in range(r, l-1, -2):
                     score += nums[i]
-                l, r = r+2, r+1
-            r += 1
+                l, r = r+2, r+2
 
         return score
