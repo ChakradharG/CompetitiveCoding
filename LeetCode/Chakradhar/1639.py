ef14a301f061cb21a183afe8ab27acde06bd228e
Chakradhar Guntuboina <47364794+ChakradharG@users.noreply.github.com>
2024-12-30 22:25:00 +0000

    1639

--- LeetCode/Chakradhar/1639.py
+++ LeetCode/Chakradhar/1639.py
@@ -1,5 +1,6 @@
 class Solution:
     def numWays(self, words: List[str], target: str) -> int:
+        MOD = 10**9 + 7
         m, n = len(target), len(words[0])
         grid = [[0 for j in range(26)] for i in range(n)]
         for word in words:
@@ -7,21 +8,18 @@ class Solution:
                 j = ord(word[i]) - 97
                 grid[i][j] += 1
 
-        def dfs(i, k):
-            if k == m:
-                return 1
-            if i == n:
-                return 0
-            key = (i, k)
-            if key not in memo:
-                j = ord(target[k]) - 97
-                res = (
-                    dfs(i + 1, k) + # skip
-                    dfs(i + 1, k + 1) * grid[i][j]  # use
-                ) % MOD
-                memo[key] = res
-            return memo[key]
+        row0 = [0 for _ in range(m + 1)]
+        row0[-1] = 1
+        row1 = [0 for _ in range(m + 1)]
+        row1[-1] = 1
 
-        memo = {}
-        MOD = 10**9 + 7
-        return dfs(0, 0)
+        for i in reversed(range(n)):
+            for k in reversed(range(min(i+1, m))):
+                j = ord(target[k]) - 97
+                row0[k] = (
+                    row1[k] + # skip
+                    row1[k+1] * grid[i][j]  # use
+                ) % MOD
+            row0, row1 = row1, row0
+
+        return row1[0]
