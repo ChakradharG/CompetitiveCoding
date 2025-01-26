07bb87e9f0ec9fca7d1ac5be7f970a7ba69faea7
Chakradhar Guntuboina <47364794+ChakradharG@users.noreply.github.com>
2025-01-26 14:32:00 +0000

    2948

--- LeetCode/Chakradhar/2948.py
+++ LeetCode/Chakradhar/2948.py
@@ -2,8 +2,11 @@ class Solution:
     def lexicographicallySmallestArray(self, nums: List[int], limit: int) -> List[int]:
         n = len(nums)
         sn = sorted(nums)
-        par = {sn[0]: 0}    # num -> index
-        ptr = {0: 0}    # index -> index
+        # sorted nums, a union will be a subarray in this with the parent 
+        # being the smallest (leftmost) element of that subarray
+
+        par = {sn[0]: 0}    # num -> index in sorted nums, parent map
+        ptr = {0: 0}    # index -> index, to keep track of the next element to use in the particular union
         l, r = 0, 1
         while r < n:
             if (sn[r] - sn[r-1]) > limit:
@@ -13,7 +16,7 @@ class Solution:
             r += 1
 
         for i in range(n):
-            p = par[nums[i]]
+            p = par[nums[i]]    # parent's index in sorted nums
             nums[i] = sn[ptr[p]]
             ptr[p] += 1
 
