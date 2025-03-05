5529449022be2aefd894eae5fa201a59f8cbc5fc
Chakradhar Guntuboina <47364794+ChakradharG@users.noreply.github.com>
2025-03-05 15:56:00 +0000

    2448

--- LeetCode/Chakradhar/2448.py
+++ LeetCode/Chakradhar/2448.py
@@ -1,25 +1,21 @@
 class Solution:
     def minCost(self, nums: List[int], cost: List[int]) -> int:
-        n, mn = len(nums), min(nums)
-        freq = {}   # num -> cost
-        for num, cst in zip(nums, cost):
-            num -= mn
-            freq[num] = cst + freq.get(num, 0)
-
-        arr = list(sorted(freq.items(), key=lambda x: x[0]))
         pre1, pre2 = 0, 0
         suf1, suf2 = 0, 0
-        for nm, cs in arr:
-            suf1 += (nm * cs)
-            suf2 += cs
+        freq = {}   # num -> cost
+        for num, cst in zip(nums, cost):
+            freq[num] = cst + freq.get(num, 0)
+            suf1 += (num * cst)
+            suf2 += cst
 
+        arr = list(sorted(freq.items(), key=lambda x: x[0]))
         ans = math.inf
-        for nm, cs in arr:
-            ans = min(ans, (suf1 - pre1) - nm*(suf2 - pre2))
-            x = nm * cs
+        for num, cst in arr:
+            ans = min(ans, (suf1 - pre1) - num*(suf2 - pre2))
+            x = num * cst
             pre1 += x
-            pre2 += cs
+            pre2 += cst
             suf1 -= x
-            suf2 -= cs
+            suf2 -= cst
 
         return ans
