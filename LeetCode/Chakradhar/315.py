fdfb7e5f40d65a0c4fd098c09865b1803da63835
Chakradhar Guntuboina <47364794+ChakradharG@users.noreply.github.com>
2025-08-04 04:29:08 +0530

    315

--- LeetCode/Chakradhar/315.py
+++ LeetCode/Chakradhar/315.py
@@ -1,13 +1,35 @@
-from sortedcontainers import SortedList
+class BIT:
+    def __init__(self, a):
+        mn, mx = math.inf, -math.inf
+        for num in a:
+            mn = min(mn, num)
+            mx = max(mx, num)
+        self.mn = mn
+        self.tree = [0 for _ in range(mx-mn+2)] # self.tree[i] = number of elements smaller than or equal to i
+
+    def query(self, i):
+        i = i - self.mn + 1
+        ans = 0
+        while i > 0:
+            ans += self.tree[i]
+            i -= (i & -i)
+        return ans
+
+    def update(self, i, d):
+        i = i - self.mn + 1
+        while i < len(self.tree):
+            self.tree[i] += d
+            i += (i & -i)
 
 class Solution:
     def countSmaller(self, nums: List[int]) -> List[int]:
-        n = len(nums)
-        ans = [0 for _ in range(n)]
-        l = SortedList()
+        bit = BIT(nums)
+        for num in nums:
+            bit.update(num, +1)
 
-        for i in range(n-1, -1, -1):
-            l.add(nums[i])
-            ans[i] = l.bisect_left(nums[i])
+        ans = []
+        for num in nums:
+            bit.update(num, -1)
+            ans.append(bit.query(num-1))
 
         return ans
