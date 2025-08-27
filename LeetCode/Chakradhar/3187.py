90d3869d5fcf7d6f22f4a4d3370f6f32c6b4e052
Chakradhar Guntuboina <47364794+ChakradharG@users.noreply.github.com>
2025-08-27 23:48:34 +0530

    3187 - BIT

--- LeetCode/Chakradhar/3187.py
+++ LeetCode/Chakradhar/3187.py
@@ -1,51 +1,19 @@
-class ST:
-    def __init__(self, a):
-        self.a = a
-        n = len(a)
-        self.tree = [0 for _ in range(4*n)]
-        self._build(0, 0, n)
+class BIT:
+    def __init__(self, n):
+        self.tree = [0 for _ in range(n)]   # range sum
 
-    def _merge(self, lst, rst):
-        return lst + rst
+    def query(self, i):
+        res = 0
+        while i > 0:
+            res += self.tree[i]
+            i -= (i & -i)
+        return res
 
-    def _build(self, i, s, e):
-        if e - s == 1:
-            self.tree[i] = self.a[s]
-        else:
-            m = s + (e - s) // 2
-            self._build(2*i+1, s, m)
-            self._build(2*i+2, m, e)
-            self.tree[i] = self._merge(
-                self.tree[2*i+1],
-                self.tree[2*i+2]
-            )
+    def update(self, i, d):
+        while i < len(self.tree):
+            self.tree[i] += d
+            i += (i & -i)
 
-    def query(self, i, s, e, l, r):
-        if s >= r or l >= e:
-            return 0
-        elif l <= s and e <= r:
-            return self.tree[i]
-        else:
-            m = s + (e - s) // 2
-            return self._merge(
-                self.query(2*i+1, s, m, l, r),
-                self.query(2*i+2, m, e, l, r)
-            )
-
-    def update(self, i, s, e, l, d):
-        if e - s == 1:
-            self.a[s] = d
-            self.tree[i] = d
-        else:
-            m = s + (e - s) // 2
-            if l < m:
-                self.update(2*i+1, s, m, l, d)
-            else:
-                self.update(2*i+2, m, e, l, d)
-            self.tree[i] = self._merge(
-                self.tree[2*i+1],
-                self.tree[2*i+2]
-            )
 
 class Solution:
     def countOfPeaks(self, nums: List[int], queries: List[List[int]]) -> List[int]:
@@ -55,7 +23,10 @@ class Solution:
         for i in range(1, n-1):
             if nums[i-1] < nums[i] > nums[i+1]:
                 peak[i] = 1
-        st = ST(peak)
+
+        bit = BIT(len(peak)+1)
+        for i in range(n):
+            bit.update(i+1, peak[i])
 
         ans = []
         for q in queries:
@@ -65,7 +36,8 @@ class Solution:
                     ans.append(0)
                 else:
                     ans.append(
-                        st.query(0, 0, n, q[1], q[2]+1)
+                        bit.query(q[2]+1)   # [1, r]
+                        - bit.query(q[1])   # [1, l)
                         - peak[q[1]]
                         - peak[q[2]]
                     )   # since first and last elements of subarray can't be peaks (even if they are peaks when the entire array is considered)
@@ -73,13 +45,16 @@ class Solution:
                 i = q[1]
                 nums[i] = q[2]
                 if 0 < i < n-1:
+                    old = peak[i]
                     peak[i] = int(nums[i-1] < nums[i] > nums[i+1])
-                    st.update(0, 0, n, i, peak[i])
+                    bit.update(i+1, peak[i] - old)
                 if i > 1:
+                    old = peak[i-1]
                     peak[i-1] = int(nums[i-2] < nums[i-1] > nums[i])
-                    st.update(0, 0, n, i-1, peak[i-1])
+                    bit.update(i, peak[i-1] - old)
                 if i < n-2:
+                    old = peak[i+1]
                     peak[i+1] = int(nums[i] < nums[i+1] > nums[i+2])
-                    st.update(0, 0, n, i+1, peak[i+1])
+                    bit.update(i+2, peak[i+1] - old)
 
         return ans
