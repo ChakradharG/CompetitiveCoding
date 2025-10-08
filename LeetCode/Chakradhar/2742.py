977de44752bb89a42423a0c29e029d9f7e4805bf
Chakradhar Guntuboina <47364794+ChakradharG@users.noreply.github.com>
2025-10-08 15:31:18 +0530

    2742

--- LeetCode/Chakradhar/2742.py
+++ LeetCode/Chakradhar/2742.py
@@ -1,20 +1,17 @@
 class Solution:
     def paintWalls(self, cost: List[int], time: List[int]) -> int:
-        def dfs(i, done):
-            if done >= n:
-                return 0
-            if i == n:
-                return math.inf
-
-            key = (i, done)
-            if key not in memo:
-                res = min(
-                    dfs(i+1, done),  # skip
-                    cost[i] + dfs(i+1, done+time[i]+1)
-                )
-                memo[key] = res
-            return memo[key]
-
-        memo = {}
         n = len(cost)
-        return dfs(0, 0)
+        row0 = [0 for _ in range(n+1)]
+        row1 = [math.inf for _ in range(n+1)]
+        row1[-1] = 0
+
+        for i in reversed(range(n)):
+            for done in range(n):
+                row0[done] = min(
+                    row1[done], # skip this
+                    cost[i] + row1[min(n, done + time[i] + 1)]  # paint this
+                    # free painter picks up `time[i]` walls while paid painter is busy with this
+                )
+            row0, row1 = row1, row0
+
+        return row1[0]
