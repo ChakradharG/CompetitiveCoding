8eb04f29050abcfaf1315431b3bdd2309b7c7b91
Chakradhar Guntuboina <47364794+ChakradharG@users.noreply.github.com>
2026-01-25 09:49:54 +0530

    3821

---
+++ LeetCode/Chakradhar/3821.py
@@ -0,0 +1,33 @@
+﻿class Solution:
+    def nthSmallest(self, n: int, k: int) -> int:
+        z = 0
+        m = 1
+        while m < n:
+            z += 1
+            m = m * (k + z) // z
+
+        ans = ['1']
+        k -= 1
+        cur = math.factorial(k+z) // (math.factorial(k) * math.factorial(z))
+        n -= (m - cur)
+        while k > 0 and z > 0:
+            cur0 = (cur * z) // (k + z)
+            if cur0 >= n:
+                cur = cur0
+                ans.append('0')
+                z -= 1
+            else:
+                n -= cur0
+                cur = (cur * k) // (k + z)
+                ans.append('1')
+                k -= 1
+
+        while k > 0:
+            ans.append('1')
+            k -= 1
+        while z > 0:
+            ans.append('0')
+            z -= 1
+
+        return int(''.join(ans), 2)
+
