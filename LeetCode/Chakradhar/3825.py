b7e21d0405a3b0ec51b5c7b02b8811fe6972e9a6
Chakradhar Guntuboina <47364794+ChakradharG@users.noreply.github.com>
2026-01-31 21:38:51 +0530

    3825

---
+++ LeetCode/Chakradhar/3825.py
@@ -0,0 +1,21 @@
+﻿class Solution:
+    def LIS(self, nums):
+        lis = []
+        for num in nums:
+            i = bisect_left(lis, num)
+            if i == len(lis):
+                lis.append(num)
+            else:
+                lis[i] = num
+        return len(lis)
+
+    def longestSubsequence(self, nums: List[int]) -> int:
+        ans = 0
+        for b in range(32):
+            temp = []
+            for num in nums:
+                if (num>>b)&0b1 != 0:
+                    temp.append(num)
+            ans = max(ans, self.LIS(temp))
+
+        return ans
