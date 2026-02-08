17760c5f53ebe577dcf24a5a9e55c8a698786d83
Chakradhar Guntuboina <47364794+ChakradharG@users.noreply.github.com>
2026-02-08 23:00:40 +0530

    3836

---
+++ LeetCode/Chakradhar/3836.py
@@ -0,0 +1,21 @@
+﻿class Solution:
+    def maxScore(self, nums1: List[int], nums2: List[int], k: int) -> int:
+        n, m = len(nums1), len(nums2)
+        grid0 = [[-math.inf for j in range(m+1)] for i in range(n+1)]
+        grid1 = [[0 for j in range(m+1)] for i in range(n+1)]
+
+        for _ in range(k):
+            grid0[-1][-1] = -math.inf
+            for i in reversed(range(n)):
+                grid0[i][-1] = -math.inf
+                for j in reversed(range(m)):
+                    grid0[-1][j] = -math.inf
+                    grid0[i][j] = max(
+                        (nums1[i] * nums2[j]) + grid1[i+1][j+1],
+                        grid0[i+1][j],
+                        grid0[i][j+1],
+                        grid0[i+1][j+1],
+                    )
+            grid0, grid1 = grid1, grid0
+
+        return grid1[0][0]
