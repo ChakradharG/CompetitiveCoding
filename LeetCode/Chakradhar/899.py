f45d29d27e45356fb8e9848f162968378005ea4d
Chakradhar Guntuboina <47364794+ChakradharG@users.noreply.github.com>
2026-02-22 16:44:41 +0530

    899

---
+++ LeetCode/Chakradhar/899.py
@@ -0,0 +1,10 @@
+﻿class Solution:
+    def orderlyQueue(self, s: str, k: int) -> str:
+        if k > 1:
+            return "".join(sorted(s))
+        ans = s
+        for i in range(1, len(s)):
+            ans = min(ans, s[i:] + s[:i])
+
+        return ans
+
