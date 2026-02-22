17e5313520d2f6776e8808e638bae8dd57247b4f
Chakradhar Guntuboina <47364794+ChakradharG@users.noreply.github.com>
2026-02-22 12:52:00 +0530

    2136

---
+++ LeetCode/Chakradhar/2136.py
@@ -0,0 +1,10 @@
+﻿class Solution:
+    def earliestFullBloom(self, plantTime: List[int], growTime: List[int]) -> int:
+        offset = 0
+        ans = 0
+        for g, p in sorted(zip(growTime, plantTime), reverse=True):
+            offset += p
+            ans = max(ans, offset + g)
+
+        return ans
+
