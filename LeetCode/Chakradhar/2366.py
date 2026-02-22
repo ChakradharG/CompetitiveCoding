29ab122b5551fb3de84c8dbbcaa3f43cac564a82
Chakradhar Guntuboina <47364794+ChakradharG@users.noreply.github.com>
2026-02-22 12:19:48 +0530

    2366

---
+++ LeetCode/Chakradhar/2366.py
@@ -0,0 +1,13 @@
+﻿class Solution:
+    def minimumReplacement(self, nums: List[int]) -> int:
+        ans = 0
+        prv = nums[-1]
+        for num in reversed(nums[:-1]):
+            if num > prv:
+                x = math.ceil(num/prv) # minimum blocks such that max is <= prv
+                ans += (x - 1) # x-1 replacements to create x blocks
+                prv = num // x # divide num equally in x blocks, since we want the first block as big as possible (<= prv)
+            else:
+                prv = num
+
+        return ans
