7136cb616fa708e744be9d6783450453d426ba36
Chakradhar Guntuboina <47364794+ChakradharG@users.noreply.github.com>
2026-02-28 22:25:15 +0530

    3855

---
+++ LeetCode/Chakradhar/3855.py
@@ -0,0 +1,13 @@
+﻿class Solution:
+    def sumOfNumbers(self, l: int, r: int, k: int) -> int:
+        MOD = 10**9 + 7
+        m = pow(r-l+1, k-1, MOD)
+        sm = pow(10, k, MOD)
+        sm = (sm - 1) % MOD
+        sm = (sm * pow(9, -1, MOD)) % MOD
+
+        ans = 0
+        for d in range(l, r+1):
+            ans = (ans + m*d*sm) % MOD
+
+        return ans
