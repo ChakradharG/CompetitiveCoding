b3a86c50daa64cecb4004b6ff939b830577cd6d5
Chakradhar Guntuboina <47364794+ChakradharG@users.noreply.github.com>
2026-03-20 15:22:53 +0530

    3873

--- LeetCode/Chakradhar/3873.py
+++ LeetCode/Chakradhar/3873.py
@@ -4,37 +4,35 @@
             aRep, bRep = find(a), find(b)
             if aRep != bRep:
                 par[bRep] = aRep
-                size[aRep] += size[bRep] - 1
 
         def find(a):
             if par[a] != a:
                 par[a] = find(par[a])
             return par[a]
 
-        Xs = defaultdict(int)
-        Ys = defaultdict(int)
-        for x, y in points:
-            Xs[x] += 1
-            Ys[y] += 1
-
         par = {}
         size = {}
-        for x, s in Xs.items():
-            k = f'x{x}'
-            par[k] = k
-            size[k] = s
-        for y, s in Ys.items():
-            k = f'y{y}'
-            par[k] = k
-            size[k] = s
-
         for x, y in points:
+            kx = f'x{x}'
+            ky = f'y{y}'
+            if kx not in par:
+                par[kx] = kx
+                size[kx] = 0
+            if ky not in par:
+                par[ky] = ky
+                size[ky] = 0
+            size[kx] += 1
+            size[ky] += 1
             union(f'x{x}', f'y{y}')
 
+        for k in par:
+            if k[0] == 'y' or par[k] == k:
+                continue
+            size[par[k]] += size[k]
+
         mx0, mx1 = 0, 0
-        for x in Xs:
-            k = f'x{x}'
-            if par[k] != k:
+        for k in par:
+            if k[0] == 'y' or par[k] != k:
                 continue
             if mx0 <= size[k]:
                 mx0, mx1 = size[k], mx0
