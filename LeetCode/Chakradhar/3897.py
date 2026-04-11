68fb7a71d6c593ec5451143aed28bcaf7497bc6c
Chakradhar Guntuboina <47364794+ChakradharG@users.noreply.github.com>
2026-04-11 22:24:49 +0530

    3897

---
+++ LeetCode/Chakradhar/3897.py
@@ -0,0 +1,19 @@
+﻿class Solution:
+    def maxValue(self, nums1: list[int], nums0: list[int]) -> int:
+        def f(x):
+            nonlocal ans
+            o, z = x
+            if z == 0:
+                # if no 0's, directly append it to ans
+                ans = ((ans << o) + (2**o - 1)) % MOD
+                return False
+            else:
+                return True
+
+        ans = 0
+        MOD = 10**9 + 7
+        for o, z in sorted(filter(f, zip(nums1, nums0)), key=lambda x: (-x[0], x[1])):
+            ans = ((ans << o) + (2**o - 1)) % MOD
+            ans = (ans << z) % MOD
+
+        return ans
