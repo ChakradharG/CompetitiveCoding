8e43be3217cfe9b2b703ed5f9699411a887becf0
Chakradhar Guntuboina <47364794+ChakradharG@users.noreply.github.com>
2026-05-03 15:01:53 +0530

    3920

---
+++ LeetCode/Chakradhar/3920.py
@@ -0,0 +1,18 @@
+﻿class Solution:
+    def maxFixedPoints(self, nums: list[int]) -> int:
+        n = len(nums)
+        poss = []
+        for i, num in enumerate(nums):
+            if num <= i:
+                poss.append((i - num, num))
+        poss.sort()
+
+        LIS = []
+        for _, num in poss:
+            i = bisect_left(LIS, num)
+            if i < len(LIS):
+                LIS[i] = num
+            else:
+                LIS.append(num)
+
+        return len(LIS)
