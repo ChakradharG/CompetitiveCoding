aa4f1a9819a4fc90a5faa15c49df3d55ee171af3
Chakradhar Guntuboina <47364794+ChakradharG@users.noreply.github.com>
2026-07-08 22:49:26 +0530

    3041

---
+++ LeetCode/Chakradhar/3041.py
@@ -0,0 +1,21 @@
+﻿class Solution:
+    def maxSelectedElements(self, nums: List[int]) -> int:
+        nums.sort()
+        ans = 1
+        psame, pinc = 1, 1
+
+        for i in range(1, len(nums)):
+            same, inc = 1, 1
+            match nums[i] - nums[i-1]:
+                case 0:
+                    same = psame
+                    inc = psame + 1
+                case 1:
+                    same = psame + 1
+                    inc = pinc + 1
+                case 2:
+                    same = pinc + 1
+            ans = max(ans, same, inc)
+            psame, pinc = same, inc
+
+        return ans
