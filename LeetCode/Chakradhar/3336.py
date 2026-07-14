47beba86faba26fcd30e9bf5280d1eb73407d1e6
Chakradhar Guntuboina <47364794+ChakradharG@users.noreply.github.com>
2026-07-15 00:23:20 +0530

    3336

---
+++ LeetCode/Chakradhar/3336.py
@@ -0,0 +1,26 @@
+﻿class Solution:
+    def subsequencePairCount(self, nums: List[int]) -> int:
+        @cache
+        def gcd(a, b):
+            while b:
+                a, b = b, a % b
+            return a
+
+        @cache
+        def dfs(i, seq1, seq2):
+            areEq = 0
+            if seq1 != 0 and seq1 == seq2:
+                areEq = 1
+            if i == n:
+                return areEq
+            return (
+                + dfs(i+1, seq1, seq2)
+                + dfs(i+1, gcd(seq1, nums[i]), seq2)
+                + dfs(i+1, seq1, gcd(seq2, nums[i]))
+            ) % MOD
+
+        n = len(nums)
+        MOD = 10**9 + 7
+
+        return dfs(0, 0, 0)
+
