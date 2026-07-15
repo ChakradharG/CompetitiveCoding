b04c8b0d0eb62d934cb3094da9f3c211b1dff06e
Chakradhar Guntuboina <47364794+ChakradharG@users.noreply.github.com>
2026-07-15 15:13:49 +0530

    3336 - Bottom-up (optimized)

--- LeetCode/Chakradhar/3336.py
+++ LeetCode/Chakradhar/3336.py
@@ -11,13 +11,24 @@
         grid = [[1 if i==j else 0 for j in range(m)] for i in range(m)]
         grid[0][0] = 0
 
-        for num in reversed(nums):
+        poss = [[]]
+        prv = set()
+        for num in nums:
+            cur = {num}
+            for x in prv:
+                cur.add(gcd(num, x))
+            prv |= cur
+            poss.append(sorted(prv))
+
+        for i in reversed(range(n)):
+            num, p = nums[i], poss[i]
             grid[0][0] = (grid[0][0] + grid[num][0] + grid[0][num]) % MOD
-            for y in reversed(range(1, m)):
+            for seq1 in reversed(p):
+                y = seq1
                 grid[y][0] = (grid[y][0] + grid[gcd(y, num)][0] + grid[y][num]) % MOD
                 grid[0][y] = (grid[0][y] + grid[num][y] + grid[0][gcd(y, num)]) % MOD
-            for seq1 in reversed(range(1, m)):
-                for seq2 in reversed(range(1, m)):
+            for seq1 in reversed(p):
+                for seq2 in reversed(p):
                     grid[seq1][seq2] = (
                         grid[seq1][seq2]
                         + grid[gcd(seq1, num)][seq2]
