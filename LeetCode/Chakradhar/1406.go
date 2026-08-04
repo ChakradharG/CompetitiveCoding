aa73ee317ba269c6f087a95059b3bb3518f29300
Chakradhar Guntuboina <47364794+ChakradharG@users.noreply.github.com>
2026-08-04 16:32:03 +0530

    1406

---
+++ LeetCode/Chakradhar/1406.go
@@ -0,0 +1,22 @@
+﻿func stoneGameIII(stoneValue []int) string {
+    n := len(stoneValue)
+    stoneValue = append(stoneValue, 0, 0)
+    dp := make([]int, n+3)
+
+    for i := n-1; i >= 0; i-- {
+        dp[i] = max(
+            stoneValue[i] - dp[i+1],
+            stoneValue[i] + stoneValue[i+1] - dp[i+2],
+            stoneValue[i] + stoneValue[i+1] + stoneValue[i+2] - dp[i+3],
+        )
+    }
+
+    if dp[0] > 0 {
+        return "Alice"
+    } else if dp[0] == 0 {
+        return "Tie"
+    } else {
+        return "Bob"
+    }
+}
+
