1742a8a09aab96cdd0de999780e7158e0b8dcc36
Chakradhar Guntuboina <47364794+ChakradharG@users.noreply.github.com>
2026-08-04 16:26:49 +0530

    1406

---
+++ LeetCode/Chakradhar/1406.py
@@ -0,0 +1,20 @@
+﻿class Solution:
+    def stoneGameIII(self, stoneValue: List[int]) -> str:
+        n = len(stoneValue)
+        stoneValue.extend([0, 0])
+
+        dp = [0] * (n + 3)
+        for i in reversed(range(n)):
+            dp[i] = max(
+                stoneValue[i] - dp[i+1],
+                stoneValue[i] + stoneValue[i+1] - dp[i+2],
+                stoneValue[i] + stoneValue[i+1] + stoneValue[i+2] - dp[i+3],
+            )
+
+        if dp[0] < 0:
+            return "Bob"
+        elif dp[0] == 0:
+            return "Tie"
+        else:
+            return "Alice"
+
