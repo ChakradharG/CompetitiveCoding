87c491ce457dd3bf220eca82ac887d5a4db3a10c
Chakradhar Guntuboina <47364794+ChakradharG@users.noreply.github.com>
2026-08-06 16:54:42 +0530

    3347

--- LeetCode/Chakradhar/3347.py
+++ LeetCode/Chakradhar/3347.py
@@ -1,30 +1,23 @@
-class Solution:
+﻿class Solution:
     def maxFrequency(self, nums: List[int], k: int, numOperations: int) -> int:
         n = len(nums)
+        freq = Counter(nums)
         if n == 1:
             return 1
         if k == 0 or numOperations == 0:
-            return sorted(Counter(nums).items(), reverse=True, key=lambda x: x[1])[0][1]
+            return max(freq.values())
 
-        h = []
-        for num in nums:
-            h.append((num+k+1, 0))
-            h.append((num+1, 1))
-            h.append((num-k, 2))
-            h.append((num, 3))
-        h.sort()
-
-        c, e = 0, 0
+        nums.sort()
         ans = 0
-        for v, d in h:
-            if d == 0:
-                c -= 1
-            elif d == 1:
-                e -= 1
-            elif d == 2:
-                c += 1
-            else:
-                e += 1
-            ans = max(ans, e + min(numOperations, c-e))
-
+        l = 0
+        for r, num in enumerate(nums):
+            # count of nums that can be converted to num
+            start = bisect_left(nums, num - k)
+            end = bisect_right(nums, num + k)
+            ans = max(ans, min(freq[num]+numOperations, end-start))
+            # count of nums that can be converted to some number in [num-k, num]
+            while num - nums[l] > 2*k:
+                l += 1
+            ans = max(ans, min(numOperations, r-l+1))
         return ans
+
