819a6f17298eb8acbcd189be0c013ee42020ef6f
Chakradhar Guntuboina <47364794+ChakradharG@users.noreply.github.com>
2026-08-10 20:58:29 +0530

    1510

---
+++ LeetCode/Chakradhar/1510.py
@@ -0,0 +1,15 @@
+﻿class Solution:
+    def winnerSquareGame(self, n: int) -> bool:
+        row = [False] * (n+1) # row[i] indicates whether the player in play when
+        # `i` stones are remaining can win or not
+        for c in range(1, n+1):
+            row[c] = not row[c-1]
+            i = math.floor(math.sqrt(c))
+            i2 = i*i
+            while i > 1 and not row[c]:
+                row[c] = not row[c-i2]
+                i2 -= (2*i - 1)
+                i -= 1
+
+        return row[n]
+
