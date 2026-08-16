4523e5584627578d9cc65018c3f07b475e990970
Chakradhar Guntuboina <47364794+ChakradharG@users.noreply.github.com>
2026-08-16 22:39:56 +0530

    1510 - precompute all vals

--- LeetCode/Chakradhar/1510.py
+++ LeetCode/Chakradhar/1510.py
@@ -1,15 +1,14 @@
-﻿class Solution:
+﻿row = [False] * (10**5+1) # row[i] indicates whether the player in play when
+# `i` stones are remaining can win or not
+for c in range(1, 10**5+1):
+    row[c] = not row[c-1]
+    i = math.floor(math.sqrt(c))
+    i2 = i*i
+    while i > 1 and not row[c]:
+        row[c] = not row[c-i2]
+        i2 -= (2*i - 1)
+        i -= 1
+
+class Solution:
     def winnerSquareGame(self, n: int) -> bool:
-        row = [False] * (n+1) # row[i] indicates whether the player in play when
-        # `i` stones are remaining can win or not
-        for c in range(1, n+1):
-            row[c] = not row[c-1]
-            i = math.floor(math.sqrt(c))
-            i2 = i*i
-            while i > 1 and not row[c]:
-                row[c] = not row[c-i2]
-                i2 -= (2*i - 1)
-                i -= 1
-
         return row[n]
-
