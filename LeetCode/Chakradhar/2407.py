2d18ef0d8c5903f4d9eb6a9087afae3eb85d30c2
Chakradhar Guntuboina <47364794+ChakradharG@users.noreply.github.com>
2026-08-18 06:18:55 +0530

    2407

--- LeetCode/Chakradhar/2407.py
+++ LeetCode/Chakradhar/2407.py
@@ -1,52 +1,46 @@
-class ST:
+﻿class ST:
     def __init__(self, n):
-        self.tree = [0 for _ in range(4*n)]
+        self.n = n
+        self.tree = [0 for _ in range(2*n)]
 
     def _merge(self, lst, rst):
         return max(lst, rst)
 
-    def query(self, i, s, e, l, r):
-        if s >= r or e <= l:
-            return 0
-        elif l <= s and e <= r:
-            return self.tree[i]
-        else:
-            m = s + (e - s) // 2
-            return self._merge(
-                self.query(2*i+1, s, m, l, r),
-                self.query(2*i+2, m, e, l, r)
-            )
+    def query(self, l, r):
+        l += self.n
+        r += self.n
+        res = 0
+        while l < r:
+            if l & 1:
+                res = self._merge(res, self.tree[l])
+                l += 1
+            if r & 1:
+                r -= 1
+                res = self._merge(res, self.tree[r])
+            l >>= 1
+            r >>= 1
+        return res
 
-    def update(self, i, s, e, l, d):
-        if l < s or e <= l:
-            return
-        elif e - s == 1 and s == l:
-            self.tree[i] = d
-        else:
-            m = s + (e - s) // 2
-            self.update(2*i+1, s, m, l, d)
-            self.update(2*i+2, m, e, l, d)
-            self.tree[i] = self._merge(
-                self.tree[2*i+1],
-                self.tree[2*i+2]
-            )
+    def update(self, l, d):
+        i = l + self.n
+        self.tree[i] = d
+        while (i := (i >> 1)) > 0:
+            x = self._merge(self.tree[2*i], self.tree[2*i+1])
+            if self.tree[i] == x:
+                break
+            self.tree[i] = x
 
 
 class Solution:
     def lengthOfLIS(self, nums: List[int], k: int) -> int:
-        unique = set()
-        for num in nums:
-            unique.add(num-k)
-            unique.add(num-1)
-            unique.add(num)
-        rev_map = {v: i for i, v in enumerate(sorted(unique))}
-        n = len(rev_map)
+        n = max(nums) + 1
         st = ST(n)
 
         ans = 0
         for num in nums:
-            x = 1 + st.query(0, 0, n, rev_map[num-k], rev_map[num])
+            x = 1 + st.query(max(0, num-k), num)
             ans = max(ans, x)
-            st.update(0, 0, n, rev_map[num], x)
+            st.update(num, x)
 
         return ans
+
