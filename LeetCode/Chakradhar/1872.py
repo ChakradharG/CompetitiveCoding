2df7860842e04dd88ab09600f6d19f528b6377e0
Chakradhar Guntuboina <47364794+ChakradharG@users.noreply.github.com>
2026-08-26 08:16:11 +0530

    1872

---
+++ LeetCode/Chakradhar/1872.py
@@ -0,0 +1,19 @@
+﻿class Solution:
+    def stoneGameVIII(self, stones: List[int]) -> int:
+        n = len(stones)
+        pref = [0] * n
+        pref[0] = stones[0]
+        for i in range(1, n):
+            pref[i] = pref[i-1] + stones[i]
+
+        score_diff = [0] * n
+        score_diff[n-1] = pref[n-1]
+
+        for i in reversed(range(1, n-1)):
+            score_diff[i] = max(
+                score_diff[i+1], # skip
+                pref[i] - score_diff[i+1] # pick
+            )
+
+        return score_diff[1]
+
