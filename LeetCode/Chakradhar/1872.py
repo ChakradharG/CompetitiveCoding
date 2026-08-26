e8a59a6f8df5655c3777cbdfe8c468df1c289d9e
Chakradhar Guntuboina <47364794+ChakradharG@users.noreply.github.com>
2026-08-26 08:36:45 +0530

    1872

--- LeetCode/Chakradhar/1872.py
+++ LeetCode/Chakradhar/1872.py
@@ -6,14 +6,12 @@
         for i in range(1, n):
             pref[i] = pref[i-1] + stones[i]
 
-        score_diff = [0] * n
-        score_diff[n-1] = pref[n-1]
-
+        score_diff = pref[n-1]
         for i in reversed(range(1, n-1)):
-            score_diff[i] = max(
-                score_diff[i+1], # skip
-                pref[i] - score_diff[i+1] # pick
+            score_diff = max(
+                score_diff, # skip
+                pref[i] - score_diff # pick
             )
 
-        return score_diff[1]
+        return score_diff
 
