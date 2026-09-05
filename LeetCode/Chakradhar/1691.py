57ff33f55beb199c7df7936ed5159c9675df37e2
Chakradhar Guntuboina <47364794+ChakradharG@users.noreply.github.com>
2026-09-05 23:31:03 +0530

    1691

--- LeetCode/Chakradhar/1691.py
+++ LeetCode/Chakradhar/1691.py
@@ -1,28 +1,19 @@
 ﻿class Solution:
     def maxHeight(self, cuboids: List[List[int]]) -> int:
-        ornt = {
-            0: (0, 1, 2), 1: (0, 2, 1), 2: (1, 2, 0),
-            3: (1, 0, 2), 4: (2, 1, 0), 5: (2, 0, 1)
-        }
-
         n = len(cuboids)
         for i in range(n):
             cuboids[i].sort(reverse=True)
         cuboids.sort(reverse=True)
 
         @cache
-        def dfs(i, j, o):
+        def dfs(i, j):
             if i == n:
                 return 0
-            res = dfs(i+1, j, o)
-            pw, pl, ph = ornt[o]
-            for no in range(6):
-                w, l, h = ornt[no]
-                if ((cuboids[i][w] <= cuboids[j][pw]) and \
-                   (cuboids[i][l] <= cuboids[j][pl]) and \
-                   (cuboids[i][h] <= cuboids[j][ph])) or j == -1:
-                    res = max(res, cuboids[i][h] + dfs(i+1, i, no))
+            res = dfs(i+1, j)
+            if ((cuboids[i][0] <= cuboids[j][0]) and \
+                (cuboids[i][1] <= cuboids[j][1]) and \
+                (cuboids[i][2] <= cuboids[j][2])) or j == -1:
+                    res = max(res, cuboids[i][0] + dfs(i+1, i))
             return res
 
-        return dfs(0, -1, 0)
-
+        return dfs(0, -1)
