cfc7d61a53471de022d85bccdb3ae2f25ef4ecb1
Chakradhar Guntuboina <47364794+ChakradharG@users.noreply.github.com>
2026-09-06 10:49:53 +0530

    1691

--- LeetCode/Chakradhar/1691.py
+++ LeetCode/Chakradhar/1691.py
@@ -5,15 +5,18 @@
             cuboids[i].sort(reverse=True)
         cuboids.sort(reverse=True)
 
-        @cache
-        def dfs(i, j):
-            if i == n:
-                return 0
-            res = dfs(i+1, j)
-            if ((cuboids[i][0] <= cuboids[j][0]) and \
-                (cuboids[i][1] <= cuboids[j][1]) and \
-                (cuboids[i][2] <= cuboids[j][2])) or j == -1:
-                    res = max(res, cuboids[i][0] + dfs(i+1, i))
-            return res
+        row0 = [0] * (n + 1)
+        row1 = [0] * (n + 1)
 
-        return dfs(0, -1)
+        for i in reversed(range(n)):
+            row0[-1] = max(row1[-1], cuboids[i][0] + row1[i])
+            for j in range(i):
+                row0[j] = row1[j] # skip
+                if (cuboids[i][0] <= cuboids[j][0]) and \
+                    (cuboids[i][1] <= cuboids[j][1]) and \
+                    (cuboids[i][2] <= cuboids[j][2]):
+                    row0[j] = max(row0[j], cuboids[i][0] + row1[i])
+            row0, row1 = row1, row0
+
+        return row1[-1]
+
