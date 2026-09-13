14ad1808d39e7038b946238f519ebc5d3ba073ce
Chakradhar Guntuboina <47364794+ChakradharG@users.noreply.github.com>
2026-09-13 23:24:27 +0530

    3977 (pareto-frontier)

--- LeetCode/Chakradhar/3977.py
+++ LeetCode/Chakradhar/3977.py
@@ -2,16 +2,17 @@
     def minTimeMaxPower(self, n: int, edges: List[List[int]], power: int, cost: List[int], source: int, target: int) -> List[int]:
         graph = [[] for _ in range(n)]
         for u, v, t in edges:
-            graph[u].append([v, t])
+            graph[u].append((v, t))
 
-        dist = [[inf for p in range(power+1)] for _ in range(n)]
-        dist[source][power] = 0
+        distT = [inf] * n
+        distP = [-inf] * n
+        distT[source] = 0
+        distP[source] = power
+
         h = [(0, -power, source)]
         while h:
             t, p, u = heappop(h)
             p *= -1
-            if t > dist[u][p]:
-                continue
             if u == target:
                 return [t, p]
             p -= cost[u]
@@ -19,9 +20,9 @@
                 continue
             for v, dt in graph[u]:
                 nt = t + dt
-                if nt < dist[v][p]:
-                    dist[v][p] = nt
+                if (nt < distT[v]) or (p > distP[v]):
+                    distT[v] = nt
+                    distP[v] = p
                     heappush(h, (nt, -p, v))
 
         return [-1, -1]
-
