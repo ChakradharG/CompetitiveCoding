0a5f4e7602a2eaf0c57853eae05d76b2250c7674
Chakradhar Guntuboina <47364794+ChakradharG@users.noreply.github.com>
2026-09-15 04:06:04 +0530

    2412

---
+++ LeetCode/Chakradhar/2412.py
@@ -0,0 +1,21 @@
+﻿class Solution:
+    def minimumMoney(self, transactions: List[List[int]]) -> int:
+        losses, profits = [], []
+        for cost, cashback in transactions:
+            if cashback < cost:
+                losses.append((cost, cashback))
+            else:
+                profits.append((cost, cashback))
+        losses.sort(key=lambda x: x[1])
+        profits.sort(key=lambda x: -x[0])
+
+        ans = cur = 0
+        for cost, cashback in losses + profits:
+            cur -= cost
+            if cur < 0:
+                ans -= cur
+                cur = 0
+            cur += cashback
+
+        return ans
+
