7f82060a2882546113e4754c1d3919993ad309f7
Chakradhar Guntuboina <47364794+ChakradharG@users.noreply.github.com>
2026-09-16 17:04:02 +0530

    1250

---
+++ LeetCode/Chakradhar/1250.py
@@ -0,0 +1,13 @@
+﻿class Solution:
+    def isGoodArray(self, nums: List[int]) -> bool:
+        def gcd(a, b):
+            while b:
+                a, b = b, a % b
+            return a
+
+        g = nums[0]
+        for num in nums:
+            g = gcd(g, num)
+
+        return g == 1
+
