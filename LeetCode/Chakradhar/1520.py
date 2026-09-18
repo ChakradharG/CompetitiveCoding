98be046d79c16d160f926d6f8a6fcdcf3c5de4ae
Chakradhar Guntuboina <47364794+ChakradharG@users.noreply.github.com>
2026-09-18 20:26:24 +0530

    1520

---
+++ LeetCode/Chakradhar/1520.py
@@ -0,0 +1,29 @@
+﻿class Solution:
+    def maxNumOfSubstrings(self, s: str) -> list[str]:
+        ls, rs = {}, {}
+        for i, c in enumerate(s):
+            if c not in ls:
+                ls[c] = i
+            rs[c] = i
+
+        ans = []
+        stack = []
+        for i, c in enumerate(s):
+            stack.append((i, c))
+            if i == rs[c]:
+                l = ls[c]
+                while stack and stack[-1][0] != l:
+                    c2 = stack[-1][1]
+                    if rs[c2] > rs[c]:
+                        ls[c] = l
+                        break
+                    l = min(l, ls[c2])
+                    stack.pop()
+                if stack and stack[-1][0] == l:
+                    ans.append(s[l:rs[c]+1])
+                    stack = []
+                else:
+                    stack.append((i, c))
+
+        return ans
+
