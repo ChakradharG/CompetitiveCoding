af37cc19b94bf466cec58aaf3709c452f19a466d
Chakradhar Guntuboina <47364794+ChakradharG@users.noreply.github.com>
2026-09-25 09:11:14 +0530

    956

---
+++ LeetCode/Chakradhar/956.py
@@ -0,0 +1,15 @@
+﻿class Solution:
+    def tallestBillboard(self, rods: list[int]) -> int:
+        @cache
+        def dfs(i, diff):
+            if i == n:
+                return -inf if diff else 0
+            return max(
+                dfs(i+1, diff),
+                rods[i] + dfs(i+1, diff+rods[i]),
+                dfs(i+1, diff-rods[i]),
+            )
+
+        n = len(rods)
+        return dfs(0, 0)
+
